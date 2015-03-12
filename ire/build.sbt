0496c3f14548d3972df75c509a58bf829d47213e
János Maginecz <wafleeee@gmail.com>
2015-03-12 17:03:02 +0100

    example tests added

--- ire/build.sbt
+++ ire/build.sbt
@@ -4,8 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.6"
 
-libraryDependencies +=  "com.typesafe.akka" % "akka-actor_2.11" % "2.4-SNAPSHOT"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+libraryDependencies +=  "com.typesafe.akka" % "akka-actor_2.11" % "2.4-SNAPSHOT"
 
 resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
---
+++ ire/src/test/scala/performance/select.scala
@@ -0,0 +1,27 @@
+package performance
+
+/**
+ * Created by janosmaginecz on 12/03/15.
+ */
+import org.scalatest.FlatSpec
+
+import scala.collection.mutable.Stack
+
+class StackSpec extends FlatSpec {
+
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop() === 2)
+    assert(stack.pop() === 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[String]
+    intercept[NoSuchElementException] {
+      emptyStack.pop()
+    }
+  }
+}
