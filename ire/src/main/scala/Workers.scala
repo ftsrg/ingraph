a42318598de11aaa3438aeb1112f931a990f4348
Janos Maginecz <wafleeee@gmail.com>
2015-03-21 17:51:35 +0100

    first draft of hashjoin added with tests

--- ire/src/main/scala/Workers.scala
+++ ire/src/main/scala/Workers.scala
@@ -1,5 +1,7 @@
+import Workers.nodeType
 import akka.actor.{Actor, ActorRef}
 
+import scala.collection.mutable
 /**
  * Created by Maginecz on 3/16/2015.
  */
@@ -14,7 +16,104 @@ class Trimmer(val next: (ChangeSet) => Unit, val selectionVector: Vector[Int]) e
       next(ChangeSet(
         positive.map(vec => selectionVector.map(i => vec(i))),
         negative.map(vec => selectionVector.map(i => vec(i)))
+        )
       )
+    }
+  }
+}
+
+import collection.mutable.MultiMap
+
+case class Primary(changeSet: ChangeSet)
+case class Secondary(changeSet: ChangeSet)
+
+class HashJoiner(val next:(ChangeSet) => Unit,
+               val primaryLength: Int, val primarySelector: Vector[Int],
+               val secondaryLength: Int, val secondarySelector: Vector[Int])
+  extends Actor {
+  var primaryValues = new mutable.HashMap[nodeType, mutable.Set[nodeType]] with MultiMap[nodeType, nodeType]
+  var secondaryValues = new mutable.HashMap[nodeType, mutable.Set[nodeType]] with MultiMap[nodeType, nodeType]
+
+  val inversePrimarySelector = Vector.range(0,primaryLength) filter( i=> !primarySelector.contains(i))
+  val inverseSecondarySelector = Vector.range(0,secondaryLength) filter( i=> !secondarySelector.contains(i))
+
+  override def receive: Actor.Receive = {
+    case Primary(ChangeSet(positive, negative)) => {
+      val joinedPositive = positive.filter( primaryVec => {
+        secondaryValues.contains(primarySelector.map(i => primaryVec(i)))
+      }).
+      map{
+        primaryVec => {
+          val key = primarySelector.map(i => primaryVec(i))
+          val rest = inversePrimarySelector.map(i => primaryVec(i))
+          secondaryValues(key).map(
+            secondaryVec => {
+              primaryVec ++ inverseSecondarySelector.map( i=>secondaryVec(i) )
+            }
+          ).toVector
+        }
+      }
+      val joinedNegative = negative.map(
+        primaryVec => {
+          val key = primarySelector.map(i => primaryVec(i))
+          secondaryValues(key).map(
+            secondaryVec => {
+              primaryVec ++ inverseSecondarySelector.map( i=>secondaryVec(i) )
+            }.toVector
+          )
+        }
+      )
+      next(ChangeSet(joinedPositive.flatten,joinedNegative.flatten))
+      positive.foreach(
+        vec => {
+          val key = primarySelector.map(i => vec(i))
+          primaryValues.addBinding(key,vec)
+        }
+      )
+      negative.foreach(
+        vec =>{
+          val key = primarySelector.map(i => vec(i))
+          primaryValues.removeBinding(key,vec)
+        }
+      )
+    }
+    case Secondary(ChangeSet(positive, negative)) => {
+      val joinedPositive = positive.map{
+        secondaryVec => {
+          val key = secondarySelector.map(i => secondaryVec(i))
+          val rest = inverseSecondarySelector.map(i => secondaryVec(i))
+          primaryValues(key).map(
+            primaryVec => {
+              primaryVec ++ inverseSecondarySelector.map( i=>secondaryVec(i) )
+            }
+          ).toVector
+        }
+      }
+      val joinedNegative = negative.map(
+        secondaryVec => {
+          val key = secondarySelector.map(i => secondaryVec(i))
+          val rest = inverseSecondarySelector.map(i => secondaryVec(i))
+          primaryValues(key).map(
+            primaryVec => {
+              primaryVec ++ inverseSecondarySelector.map( i=>secondaryVec(i) )
+            }.toVector
+          )
+        }
+      )
+
+      next(ChangeSet(joinedPositive.flatten,joinedNegative.flatten))
+
+      positive.foreach(
+        vec => {
+          val key = primarySelector.map(i => vec(i))
+          secondaryValues.addBinding(key,vec)
+        }
+      )
+      negative.foreach(
+        vec =>{
+          val key = primarySelector.map(i => vec(i))
+          secondaryValues.removeBinding(key,vec)
+        }
       )
     }
   }
---
+++ ire/src/test/scala/HashJoinerTest.scala
@@ -0,0 +1,40 @@
+/**
+ * Created by Maginecz on 3/21/2015.
+ */
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers, WordSpecLike}
+
+class HashJoinerTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("MySpec"))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A HasJoin" must {
+    "join the values" in {
+      val prim = ChangeSet(
+        positive = Vector(Vector(15, 16, 17, 18), Vector(4, 5, 6, 7))
+      )
+      val sec = ChangeSet(
+        positive = Vector(Vector(13,15,16))
+      )
+      val primarySel = Vector(0, 1)
+      val secondarySel = Vector(1,2)
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val joiner = system.actorOf(Props(new HashJoiner(echoActor ! _,4, primarySel, 3, secondarySel)), name = "testSelector")
+
+      joiner ! Primary(prim)
+      expectMsg(ChangeSet())
+      joiner ! Secondary(sec)
+      expectMsg(ChangeSet(
+        positive = Vector(Vector(15, 16, 17, 18, 13))
+      ))
+
+
+    }
+  }
+}
