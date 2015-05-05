25b9db6c9e3802a8d0d561959f8978c372d22106
János Maginecz <wafleeee@gmail.com>
2015-05-05 17:20:59 +0200

    JenaRDFReader parameter function definition changed to better reflect the usage

--- ire/src/main/scala/RDFReader.scala
+++ ire/src/main/scala/RDFReader.scala
@@ -8,14 +8,13 @@ import org.w3.banana.jena.JenaModule
  * Created by Maginecz on 3/4/2015.
  */
 
-class JenaRDFReader(val lookup: Map[String, Any => Unit]) extends JenaModule {
+class JenaRDFReader(val lookup: Map[String, (String,AnyRef) => Unit] = Map()) extends JenaModule {
   // reading from a stream can fail so in real life, you would have to deal with the Try[Rdf#Graph]
 
   import ops._
 
   def read(stream: Reader) {
     val graph: Rdf#Graph = turtleReader.read(stream, base = "") getOrElse sys.error("couldn't read stream")
-
     graph.triples.foreach {
       case ops.Triple(subj, pred, obj: Node_Literal) => {
         lookup(pred.getLocalName)(subj.getLocalName, obj.getLiteralValue)
--- ire/src/test/scala/BananaRDFTest.scala
+++ ire/src/test/scala/BananaRDFTest.scala
@@ -12,21 +12,19 @@ class RdfReaderTest extends FlatSpec {
       "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> ." +
       ":_1 a :Signal ;\n\t:Signal_currentState :SIGNALSTATE_GO ." +
       ":_3 a :Route ;\n\t:Route_exit :_2 .")
-    val signalStateChecker = (a: Any) => a match {
-      case (a: String, b: String) => assert(a == "_1" && b == "SIGNALSTATE_GO")
+    val signalStateChecker = (a:String, b: Any) => {
+      assert(a == "_1" && b == "SIGNALSTATE_GO")
     }
-    val routeExitChecker = (a: Any) => a match {
-      case (a: String, b: String) => assert(a == "_3" && b == "_2")
+    val routeExitChecker = (a: String, b: Any) => {
+      assert(a == "_3" && b == "_2")
     }
-    val typeChecker = (a: Any) => a match {
-      case (a: String, b: String) => {
+    val typeChecker = (a: String, b: Any) => {
         if (a == "_1")
           assert(b == "Signal")
         else if (a == "_3")
           assert(b == "Route")
         else
           fail("invalid type")
-      }
     }
     val noop = (a: Any) => ()
     val lookup = HashMap(
