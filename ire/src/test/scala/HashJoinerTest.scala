d58f17ca143bfcb63aa2d721b90114b849462ac3
János Maginecz <wafleeee@gmail.com>
2015-09-12 20:49:07 +0200

    add new hash join tests

--- ire/src/test/scala/HashJoinerTest.scala
+++ ire/src/test/scala/HashJoinerTest.scala
@@ -4,7 +4,7 @@
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActorRef, TestActors, TestKit}
-import hu.bme.mit.IQDcore.{Secondary, Primary, HashJoiner, ChangeSet}
+import hu.bme.mit.IQDcore._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 class HashJoinerTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
@@ -80,8 +80,70 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
       expectMsg(ChangeSet(
         positive = Vector(Vector(15, 16, 17, 18, 13))
       ))
+    }
+    "do join 1" in {
+      val primarySel = Vector(2)
+      val secondarySel = Vector(0)
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val joiner = system.actorOf(Props(new HashJoiner(echoActor ! _, 3, primarySel, 2, secondarySel)))
 
+      joiner ! Primary(ChangeSet(
+          positive = Vector(Vector(5,6,7),Vector(10,11,7))
+        )
+      )
+      expectMsg(ChangeSet())
 
+      joiner ! Secondary(ChangeSet(positive = Vector(Vector(7, 8))))
+      expectMsgAnyOf(utils.changeSetPermutations(ChangeSet(
+        positive = Vector(Vector(10,11,7,8),Vector(5,6,7,8))
+          )
+        ):_*
+      )
+    }
+    "do join 2" in {
+      val primarySel = Vector(1)
+      val secondarySel = Vector(0)
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val joiner = system.actorOf(Props(new HashJoiner(echoActor ! _, 2, primarySel, 2, secondarySel)))
+
+      joiner ! Primary(ChangeSet(
+        positive = Vector(Vector(1, 5),Vector(2, 6))
+        )
+      )
+      expectMsg(ChangeSet())
+
+      joiner ! Secondary(ChangeSet(
+        positive = Vector(Vector(5, 10))
+        )
+      )
+      expectMsg(ChangeSet(positive = Vector(Vector(1, 5, 10))))
+    }
+    "do join new 1" in {
+      val primarySel = Vector(1)
+      val secondarySel = Vector(0)
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val joiner = system.actorOf(Props(new HashJoiner(echoActor ! _, 2, primarySel, 2, secondarySel)))
+
+      joiner ! Primary(ChangeSet(
+        positive = Vector(Vector(2, 4),Vector(3, 4))
+      )
+      )
+      expectMsg(ChangeSet())
+
+      joiner ! Secondary(ChangeSet(
+        positive = Vector(Vector(4, 5))
+      )
+      )
+      expectMsgAnyOf(utils.changeSetPermutations(ChangeSet(positive = Vector(Vector(2, 4, 5), Vector(3, 4, 5)))):_*)
+
+      joiner ! Primary(ChangeSet(negative = Vector(Vector(3, 4))))
+      expectMsg(ChangeSet(negative = Vector(Vector(3, 4, 5))))
+      joiner ! Primary(ChangeSet(positive = Vector(Vector(3, 4))))
+      expectMsg(ChangeSet(positive = Vector(Vector(3, 4, 5))))
+      joiner ! Secondary(ChangeSet(negative = Vector(Vector(4, 5))))
+      expectMsgAnyOf(utils.changeSetPermutations(ChangeSet(negative = Vector(Vector(2, 4, 5), Vector(3, 4, 5)))):_*)
+      joiner ! Secondary(ChangeSet(positive = Vector(Vector(4, 5))))
+      expectMsgAnyOf(utils.changeSetPermutations(ChangeSet(positive = Vector(Vector(2, 4, 5), Vector(3, 4, 5)))):_*)
     }
   }
 }
