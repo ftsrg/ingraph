e07a23c7f709096dd953522baa81a9855634f539
János Maginecz <wafleeee@gmail.com>
2015-09-16 12:38:02 +0200

    fix termination protocol
    It was possible for one route of messages to reach the production node before terminators from an other route were receive. The fixed solution pauses the changeSet processing in the input nodes until every terminator reaches production.

--- ire/src/test/scala/TerminatorTest.scala
+++ ire/src/test/scala/TerminatorTest.scala
@@ -25,15 +25,21 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
       val echoActor = system.actorOf(TestActors.echoActorProps)
       val production = system.actorOf(Props(new Production("")))
       val intermediary = system.actorOf(Props(new Checker(production ! _, c => true)))
-      val input = system.actorOf(Props(new Checker(intermediary ! _, c => true)))
-      input ! ChangeSet(positive = Vector(Vector(15)))
-      input ! ChangeSet(positive = Vector(Vector(19)))
-      val terminator = Terminator(List(input ! _), production)
+      val input1 = system.actorOf(Props(new Checker(production ! _, c => true)))
+      input1 ! ChangeSet(positive = Vector(Vector(15)))
+      input1 ! ChangeSet(positive = Vector(Vector(19)))
+      val input2 = system.actorOf(Props(new Checker(intermediary ! _, c => true)))
+      input2 ! ChangeSet(positive = Vector(Vector(25)))
+      input2 ! ChangeSet(positive = Vector(Vector(29)))
+      val input3 = system.actorOf(Props(new Checker(intermediary ! _, c => true)))
+      val terminator = Terminator(List(input3 ! _, input1 ! _, input2 ! _), production)
       val future = terminator.send
-      input ! ChangeSet(positive = Vector(Vector(16)))
-      input ! ChangeSet(positive = Vector(Vector(17)))
-      val expected = Set(Vector(15),Vector(19))
-      Await.result(future, Duration(1,SECONDS)) == expected
+      input1 ! ChangeSet(positive = Vector(Vector(16)))
+      input1 ! ChangeSet(positive = Vector(Vector(17)))
+      input2 ! ChangeSet(positive = Vector(Vector(26)))
+      input2 ! ChangeSet(positive = Vector(Vector(27)))
+      val expected = Set(Vector(15),Vector(19), Vector(25), Vector(29))
+      assert(Await.result(future, Duration(1,HOURS)) == expected)
     }
   }
   "beta nodes" must {
@@ -41,17 +47,21 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
       val echoActor = system.actorOf(TestActors.echoActorProps)
       val production = system.actorOf(Props(new Production("")))
       val intermediary = system.actorOf(Props(new HashJoiner(production ! _,1,Vector(0),1,Vector(0))))
-      val input = system.actorOf(Props(new HashJoiner(intermediary ! Secondary(_),1,Vector(0), 1,Vector(0))))
-      input ! Primary(ChangeSet(positive = Vector(Vector(15))))
-      input ! Secondary(ChangeSet(positive = Vector(Vector(15))))
+      val input1 = system.actorOf(Props(new HashJoiner(intermediary ! Secondary(_),1,Vector(0), 1,Vector(0))))
+      input1 ! Primary(ChangeSet(positive = Vector(Vector(15))))
+      input1 ! Secondary(ChangeSet(positive = Vector(Vector(15))))
       intermediary ! Primary(ChangeSet(positive = Vector(Vector(15))))
-      val terminator = Terminator(List(input ! _), production)
-      val expected = Set(Vector(15))
+      val input2 = system.actorOf(Props(new HashJoiner(intermediary ! Secondary(_),1,Vector(0), 1,Vector(0))))
+      input2 ! Primary(ChangeSet(positive = Vector(Vector(25))))
+      input2 ! Secondary(ChangeSet(positive = Vector(Vector(25))))
+      intermediary ! Primary(ChangeSet(positive = Vector(Vector(25))))
+      val terminator = Terminator(List(input1 ! _, input2 ! _), production)
+      val expected = Set(Vector(15), Vector(25))
       val future = terminator.send
-      input ! Primary(ChangeSet(positive = Vector(Vector(16))))
-      input ! Secondary(ChangeSet(positive = Vector(Vector(16))))
+      input1 ! Primary(ChangeSet(positive = Vector(Vector(16))))
+      input1 ! Secondary(ChangeSet(positive = Vector(Vector(16))))
       intermediary ! Primary(ChangeSet(positive = Vector(Vector(16))))
-      Await.result(future, Duration(1,SECONDS)) == expected
+      assert(Await.result(future, Duration(1,HOURS)) == expected)
     }
   }
 }
