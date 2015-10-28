b7374732685046b05ba9dbcaf99e3a870adcfdf9
János Maginecz <wafleeee@gmail.com>
2015-10-28 22:01:10 +0100

    remove scripts for previous benchmarks

--- ire/benchmark_messagesize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for input in ${@:2}
-do
-    echo "----"
-    echo $input
-    sbt "run-main Queries $1 $input" --warn
-done
--- ire/serialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for input in $@
-do
-    echo "----"
-    echo $input
-	len=$((${#input}-3))
-    out=${input:0:$len}bin
-    sbt "run-main Serializer $input $out" --warn
-done
