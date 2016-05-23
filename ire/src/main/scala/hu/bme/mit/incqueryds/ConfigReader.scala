158b7d0f0e045130b66f1f332a619d13104baaf4
János Maginecz <wafleeee@gmail.com>
2016-05-23 18:03:21 +0200

    add yaml config reader with tests
    the reader creates a trainbenchmark query from a yaml configuration file by instantiating nodes
    using the "type" parameter in the config and passes the other keys as arguments to the node

---
+++ ire/src/main/scala/hu/bme/mit/incqueryds/ConfigReader.scala
@@ -0,0 +1,81 @@
+package hu.bme.mit.incqueryds
+
+import java.io.{FileInputStream, InputStream}
+
+import akka.actor.Props
+import com.twitter.util.Eval
+import hu.bme.mit.incqueryds.trainbenchmark.TrainbenchmarkQuery
+import org.yaml.snakeyaml.Yaml
+
+import scala.io.Source
+/**
+  * Created by wafle on 5/14/2016.
+  */
+object ConfigReader {
+  def parse(name: String, configStream: InputStream): TrainbenchmarkQuery = {
+    val yaml = new Yaml()
+    val all = yaml.load(configStream).asInstanceOf[java.util.Map[String, Object]]
+    import collection.JavaConversions._
+    val queryInput = all("input").asInstanceOf[java.util.Map[String, java.util.ArrayList[String]]].map(kv => kv._1 -> kv._2.toList).toMap
+    val network = (all - "input").asInstanceOf[scala.collection.mutable.Map[String, java.util.Map[String, String]]]
+    val queryString = generatePreamble(name) +
+      (for ((nodeName, params) <- network)
+            yield generateNode(nodeName, params.toMap)).mkString("\n") +
+       generateInputLookup(queryInput) +
+       generateInputNodes(queryInput) +
+       generateEnding()
+    print(queryString)
+    return compile(name, queryString)
+  }
+
+  def main(args: Array[String]) {
+    val data = new FileInputStream("config.yaml")
+    val yaml = new Yaml
+    import collection.JavaConversions._
+    val javaObj = yaml.load(data).asInstanceOf[java.util.Map[String, java.util.Map[String, String]]]
+    val scalaObj = javaObj.toMap.map(v => v._1 -> v._2.toMap)
+
+  }
+
+  def generateNode(name: String, params: Map[String, String]): String = {
+    val functionParams = params - "type" map (kv => s"${kv._1}=${kv._2}") mkString ", "
+    s"  val $name = newLocal(Props(new ${params("type")}($functionParams)))\n"
+  }
+
+  def generateInputLookup(input: Map[String, List[String]]): String = {
+    "  override val inputLookup = Map(" +
+    (for ((name, nodes) <- input)
+      yield s""""$name" -> ((cs: ChangeSet) => {""" +
+        (for (node <- nodes)
+          yield s"$node(cs)").mkString("\n") +
+        "})"
+      ).mkString(",\n") +
+    ")\n"
+  }
+
+  def generateInputNodes(input: Map[String, List[String]]): String = {
+    s"  val inputNodes: List[ReteMessage => Unit] = ${input.flatMap(kv => kv._2)}\n"
+  }
+  def generatePreamble(name: String): String = {
+    s"""import hu.bme.mit.incqueryds._
+        |import hu.bme.mit.incqueryds.utils.conversions._
+        |import hu.bme.mit.incqueryds.trainbenchmark.TrainbenchmarkQuery
+        |import akka.actor.Props
+        |
+        |new TrainbenchmarkQuery {
+        |  val production = newLocal(Props(new Production("$name")))
+        |""".stripMargin
+  }
+
+  def generateEnding(): String = {
+    """  override val terminator = Terminator(inputNodes, production)
+      |}
+    """.stripMargin
+  }
+
+  val importStr = "import hu.bme.mit.incqueryds.nodeType\n"
+  def compile(name: String, str: String): TrainbenchmarkQuery = {
+    val eval = new Eval()
+    eval[TrainbenchmarkQuery](str)
+  }
+}
---
+++ ire/src/test/scala/ConfigReaderTest.scala
@@ -0,0 +1,68 @@
+import java.io.ByteArrayInputStream
+
+import hu.bme.mit.incqueryds.{ChangeSet, ConfigReader}
+import org.scalatest.FlatSpec
+
+import scala.io.Source
+
+/**
+  * Created by wafle on 5/23/2016.
+  */
+class ConfigReaderTest  extends FlatSpec {
+  "alpha nodes configs should work" should "work" in {
+    val config = """f1:
+      |  type: Checker
+      |  condition: "(n) => { n(0) == 5L }"
+      |  next: production
+      |input:
+      |  test: [f1]
+    """.stripMargin
+    val engine = ConfigReader.parse("testQuery", new ByteArrayInputStream(config.getBytes("UTF-8")));
+    val testInput = engine.inputLookup("test")
+    testInput(ChangeSet(positive = Vector(Vector(0L), Vector(5L))))
+    assert(engine.getResults() == Set(Vector(5L)))
+  }
+
+  "beta nodes configs" should "work" in {
+    val config = """j1:
+                   |  type: HashJoiner
+                   |  primaryLength: 1
+                   |  primarySelector: Vector(0)
+                   |  secondaryLength: 1
+                   |  secondarySelector: Vector(0)
+                   |  next: production
+                   |input:
+                   |  pri: [j1.primary]
+                   |  sec: [j1.secondary]
+                 """.stripMargin
+    val engine = ConfigReader.parse("testQuery", new ByteArrayInputStream(config.getBytes("UTF-8")));
+    val primaryInput = engine.inputLookup("pri")
+    val secondaryInput = engine.inputLookup("sec")
+    primaryInput(ChangeSet(positive = Vector(Vector(0L), Vector(5L))))
+    secondaryInput(ChangeSet(positive = Vector(Vector(1L), Vector(5L))))
+    assert(engine.getResults() == Set(Vector(5L)))
+  }
+  "multi node configs" should "work" in {
+    val config = """j1:
+                   |  type: HashJoiner
+                   |  primaryLength: 1
+                   |  primarySelector: Vector(0)
+                   |  secondaryLength: 1
+                   |  secondarySelector: Vector(0)
+                   |  next: production
+                   |f1:
+                   |  type: Checker
+                   |  condition: "(n) => { n(0) == 5L }"
+                   |  next: j1.primary
+                   |input:
+                   |  fIn: [f1]
+                   |  sec: [j1.secondary]
+                 """.stripMargin
+    val engine = ConfigReader.parse("testQuery", new ByteArrayInputStream(config.getBytes("UTF-8")));
+    val primaryInput = engine.inputLookup("fIn")
+    val secondaryInput = engine.inputLookup("sec")
+    primaryInput(ChangeSet(positive = Vector(Vector(1L), Vector(5L))))
+    secondaryInput(ChangeSet(positive = Vector(Vector(1L), Vector(5L))))
+    assert(engine.getResults() == Set(Vector(5L)))
+  }
+}
