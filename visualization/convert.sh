b8473132b5cd1b1f343a3cc6f866b7880bfc036b
Gabor Szarnyas <szarnyasg@gmail.com>
2016-09-02 19:02:21 +0200

    Suppressed TeX output

--- visualization/convert.sh
+++ visualization/convert.sh
@@ -3,8 +3,8 @@
 echo > index.html
 for doc in *.tex; do
   filename="${doc%.*}"
-  echo ========================================
-  pdflatex -shell-escape -interaction=batchmode $filename.tex
+  echo $filename
+  pdflatex -shell-escape -interaction=batchmode $filename.tex 1>/dev/null
   echo "<h1>$filename</h1>" >> index.html
   echo "<img src='$filename.png' />" >> index.html;
 done
