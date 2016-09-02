eaa3bc34a0d9ee66e21fe69f504dc0f257015604
Gabor Szarnyas <szarnyasg@gmail.com>
2016-09-02 11:07:36 +0200

    Changed xelatex to pdflatex as it's 3x faster

--- visualization/convert.sh
+++ visualization/convert.sh
@@ -2,9 +2,9 @@
 
 echo > index.html
 for doc in *.tex; do
-	filename="${doc%.*}"
+  filename="${doc%.*}"
   echo ========================================
-  xelatex -shell-escape -interaction=batchmode $filename.tex
+  pdflatex -shell-escape -interaction=batchmode $filename.tex
   echo "<h1>$filename</h1>" >> index.html
-	echo "<img src='$filename.png' />" >> index.html;
+  echo "<img src='$filename.png' />" >> index.html;
 done
