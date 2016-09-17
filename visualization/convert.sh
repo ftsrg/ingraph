f3c0945d7d4fbab60527ddd708ed1bd161c0ab6d
Gabor Szarnyas <szarnyasg@gmail.com>
2016-09-17 09:55:26 +0200

    Updated conversion script to only convert files that were updated since the last conversion

--- visualization/convert.sh
+++ visualization/convert.sh
@@ -3,8 +3,15 @@
 echo > index.html
 for doc in *.tex; do
   filename="${doc%.*}"
-  echo $filename
-  pdflatex -shell-escape -interaction=batchmode $filename.tex 1>/dev/null
+  texfile="$filename.tex"
+  pdffile="$filename.pdf"
+
+  # only recompile files where the tex source is newer than the generated pdf
+  #echo Comparing the timestamp of $texfile and $pdffile
+  if [ $texfile -nt $pdffile ]; then
+    echo Compiling $texfile
+    pdflatex -shell-escape -interaction=batchmode $filename.tex 1>/dev/null
+  fi
   echo "<h1>$filename</h1>" >> index.html
   echo "<img src='$filename.png' />" >> index.html;
 done
