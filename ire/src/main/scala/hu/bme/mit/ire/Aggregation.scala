ded8d441c91aa6b788964f8045a97ab2318ebed4
János Maginecz <wafleeee@gmail.com>
2016-10-23 22:39:30 +0200

    Add counter

---
+++ ire/src/main/scala/hu/bme/mit/ire/Aggregation.scala
@@ -0,0 +1,65 @@
+package hu.bme.mit.ire
+import scala.collection.immutable.VectorBuilder
+import scala.collection.mutable
+import scala.util.Random
+
+trait Aggregator {
+
+}
+
+class Counter(override val next: (ReteMessage) => Unit,
+              val keys: Vector[Any],
+              override val expectedTerminatorCount:Int = 1) extends AlphaNode with SingleForwarder {
+  val counts = new mutable.HashMap[Vector[Any], Int].withDefault(d => 0)
+  override def onChangeSet(changeSet: ChangeSet): Unit = {
+    val oldValues = new mutable.HashMap[Vector[Any], Int]
+
+    for (tuple <- changeSet.positive;
+         key = keys.map(tuple(_))) {
+      if (!oldValues.contains(key)) {
+        oldValues(key) = counts(key)
+      }
+      counts(key) += 1
+    }
+
+    for (tuple <- changeSet.negative;
+         key = keys.map(tuple(_))) {
+      if (!oldValues.contains(key)) {
+        oldValues(key) = counts(key)
+      }
+      counts(key) -= 1
+    }
+
+
+    val positive = new VectorBuilder[TupleType]
+    val negative = new VectorBuilder[TupleType]
+    for ((key, oldValue) <- oldValues) {
+      if (oldValue != 0) {
+        negative += Map("count" -> oldValues(key)) ++ key.zipWithIndex.map(kv => keys(kv._2) -> kv._1).toMap
+      }
+      if (counts(key) != 0) {
+        positive += Map("count" -> counts(key)) ++ key.zipWithIndex.map(kv => keys(kv._2) -> kv._1).toMap
+      }
+    }
+
+    forward(ChangeSet(positive = positive.result(), negative = negative.result()))
+  }
+}
+
+class Average(override val next: (ReteMessage) => Unit,
+            val keys: Vector[Any],
+            override val expectedTerminatorCount:Int = 1) extends AlphaNode with SingleForwarder {
+  var count = 0
+  override def onChangeSet(changeSet: ChangeSet): Unit = {
+
+  }
+}
+
+class Sum(override val next: (ReteMessage) => Unit,
+            val keys: Vector[Any],
+            override val expectedTerminatorCount:Int = 1) extends AlphaNode with SingleForwarder {
+
+  override def onChangeSet(changeSet: ChangeSet): Unit = {
+
+  }
+}
---
+++ ire/src/test/scala/AggregationTest.scala
@@ -0,0 +1,39 @@
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import hu.bme.mit.ire.{ChangeSet, Checker, _}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class AggregationTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("MySpec"))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val odin: TupleType = Map("city" -> "Asgard", "name" -> "Odin", "weapon" -> "Gungnir", "sex" -> "male")
+  val thor: TupleType = Map("city" -> "Asgard", "name" -> "Thor", "weapon" -> "Mjölnir", "sex" -> "male")
+  val freya: TupleType = Map("city" -> "Asgard", "name" -> "Freya", "weapon" -> "Mjölnir", "sex" -> "female")
+  val geirröd: TupleType = Map("city" -> "Jötunheimr", "name" -> "Geirröd ", "sex" -> "male")
+
+  "Count" must {
+    "count with complex keys" in {
+
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val counter = system.actorOf(Props(new Counter(echoActor ! _, Vector("city", "sex"))))
+      counter ! ChangeSet(positive = Vector(odin))
+      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 1))))
+      counter ! ChangeSet(positive = Vector(thor))
+      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 2)),
+                          negative = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 1))))
+      counter ! ChangeSet(negative = Vector(odin))
+      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 1)),
+                          negative = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 2))))
+      counter ! ChangeSet(positive = Vector(freya))
+      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "female", "city" -> "Asgard", "count" -> 1))))
+      counter ! ChangeSet(negative = Vector(freya))
+      expectMsg(ChangeSet(negative = Vector(Map("sex" -> "female", "city" -> "Asgard", "count" -> 1))))
+    }
+  }
+}
