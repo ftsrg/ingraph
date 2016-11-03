bf1327083fc11a4cb4290b712f4aedc0a1b1bebb
János Maginecz <wafleeee@gmail.com>
2016-11-03 22:29:26 +0100

    implement summer

--- ire/src/test/scala/AggregationTest.scala
+++ ire/src/test/scala/AggregationTest.scala
@@ -1,7 +1,10 @@
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActors, TestKit}
 import hu.bme.mit.ire.{ChangeSet, Checker, _}
+import org.scalactic.TolerantNumerics
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.concurrent.duration.Duration
 
 class AggregationTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
@@ -12,10 +15,14 @@ class AggregationTest(_system: ActorSystem) extends TestKit(_system) with Implic
     TestKit.shutdownActorSystem(system)
   }
 
-  val odin: TupleType = Map("city" -> "Asgard", "name" -> "Odin", "weapon" -> "Gungnir", "sex" -> "male")
-  val thor: TupleType = Map("city" -> "Asgard", "name" -> "Thor", "weapon" -> "Mjölnir", "sex" -> "male")
-  val freya: TupleType = Map("city" -> "Asgard", "name" -> "Freya", "weapon" -> "Mjölnir", "sex" -> "female")
-  val geirröd: TupleType = Map("city" -> "Jötunheimr", "name" -> "Geirröd ", "sex" -> "male")
+  val odin: TupleType = Map("city" -> "Asgard", "name" -> "Odin", "weapon" -> "Gungnir", "sex" -> "male", "height" -> 1)
+  val thor: TupleType = Map(
+    "city" -> "Asgard", "name" -> "Thor", "weapon" -> "Mjölnir", "sex" -> "male", "height" -> 1.1f)
+  val freya: TupleType =
+    Map("city" -> "Asgard", "name" -> "Freya", "sex" -> "female", "height" ->  1.1)
+  val geirröd: TupleType = Map("city" -> "Jötunheimr", "name" -> "Geirröd ", "sex" -> "male", "height" -> 2L)
+  val ragnar: TupleType = Map(
+    "city" -> "Midgard", "name" -> "Ragnarr Loðbrók", "sex" -> "male", "height" -> 0.8)
 
   "Count" must {
     "count with complex keys" in {
@@ -36,4 +43,38 @@ class AggregationTest(_system: ActorSystem) extends TestKit(_system) with Implic
       expectMsg(ChangeSet(negative = Vector(Map("sex" -> "female", "city" -> "Asgard", "count" -> 1))))
     }
   }
+
+
+  def assertNextChangeset(key: Any, positive: Option[Any] = None, negative: Option[Any] = None): Unit = {
+    val cs = receiveOne(Duration.Inf).asInstanceOf[ChangeSet]
+
+    def assertEquals(actual: Any, expected: Any) {
+      (actual, expected) match {
+        case (actual: Double, expected: Double) => actual should be (expected +- 0.01)
+        case (actual: Float, expected: Float) => actual should be (expected +- 0.01f)
+        case _=> actual should be (expected)
+      }
+    }
+    if (positive.isDefined) {
+      assertEquals(cs.positive(0)(key), positive.get)
+    }
+
+    if (negative.isDefined) {
+      assertEquals(cs.negative(0)(key), negative.get)
+    }
+  }
+
+  "Sum" must {
+    "sum with complex keys" in {
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val counter = system.actorOf(Props(new Summer(echoActor ! _, Vector("sex"), "height", "sum")))
+      counter ! ChangeSet(positive = Vector(odin))
+      assertNextChangeset(key = "sum", positive = Some(1))
+      counter ! ChangeSet(positive = Vector(thor))
+      assertNextChangeset(key = "sum", positive = Some(2.1f), negative = Some(1))
+      counter ! ChangeSet(positive = Vector(ragnar))
+      assertNextChangeset(key = "sum", positive = Some(2.9), negative = Some(2.1f))
+    }
+  }
+
 }
