b4a64031bfc63509f9b5af966e5d530dd1a974cf
János Maginecz <wafleeee@gmail.com>
2016-11-03 22:29:26 +0100

    implement summer

---
+++ ire/src/main/scala/hu/bme/mit/ire/GenericMath.scala
@@ -0,0 +1,67 @@
+package hu.bme.mit.ire
+
+object GenericMath {
+  def plus(a: Any, b: Any): Any = a match {
+  case a: Float =>
+    b match {
+    case b: Float => a + b
+    case b: Double => a + b
+    case b: Int => a + b
+    case b: Long => a + b
+    }
+
+  case a: Double =>
+    b match {
+    case b: Float => a + b
+    case b: Double => a + b
+    case b: Int => a + b
+    case b: Long => a + b
+    }
+  case a: Int =>
+    b match {
+    case b: Float => a + b
+    case b: Double => a + b
+    case b: Int => a + b
+    case b: Long => a + b
+    }
+  case a: Long =>
+    b match {
+    case b: Float => a + b
+    case b: Double => a + b
+    case b: Int => a + b
+    case b: Long => a + b
+    }
+  }
+
+  def minus(a: Any, b: Any): Any = a match {
+  case a: Float =>
+    b match {
+    case b: Float => a - b
+    case b: Double => a - b
+    case b: Int => a - b
+    case b: Long => a - b
+    }
+
+  case a: Double =>
+    b match {
+    case b: Float => a - b
+    case b: Double => a - b
+    case b: Int => a - b
+    case b: Long => a - b
+    }
+  case a: Int =>
+    b match {
+    case b: Float => a - b
+    case b: Double => a - b
+    case b: Int => a - b
+    case b: Long => a - b
+    }
+  case a: Long =>
+    b match {
+    case b: Float => a - b
+    case b: Double => a - b
+    case b: Int => a - b
+    case b: Long => a - b
+    }
+  }
+}
