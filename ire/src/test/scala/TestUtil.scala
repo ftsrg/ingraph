b8cda59e4933b0c2699cc6d40c3023c57a4106b9
Gabor Szarnyas <szarnyasg@gmail.com>
2016-11-27 18:42:42 -0500

    Apply the tuples() method in tests where possible. Fixes FTSRG/ire#10

--- ire/src/test/scala/TestUtil.scala
+++ ire/src/test/scala/TestUtil.scala
@@ -1,5 +1,5 @@
 import hu.bme.mit.ire.TupleType
 
 object TestUtil {
-  def tuple(elements: Int*): TupleType = elements.zipWithIndex.map(t => t._2 -> t._1 ).toMap
+  def tuple(elements: Any*): TupleType = elements.zipWithIndex.map(t => t._2 -> t._1 ).toMap
 }
