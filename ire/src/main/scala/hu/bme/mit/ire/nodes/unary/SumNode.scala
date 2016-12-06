2c5ce95bdaf2ffe617fffbf2ec55cacf18ee3c8d
János Maginecz <wafleeee@gmail.com>
2016-12-06 20:02:39 +0100

    Repair aggregation nodes

--- ire/src/main/scala/hu/bme/mit/ire/nodes/unary/SumNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/unary/SumNode.scala
@@ -9,39 +9,39 @@ import scala.collection.immutable.VectorBuilder
 import scala.collection.mutable
 
 class SumNode(override val next: (ReteMessage) => Unit,
-              val aggregationKeys: Vector[Any], val sumKey: Any, as: String) extends UnaryNode with SingleForwarder {
+              val aggregationKeys: Vector[Int], val sumKey: Int) extends UnaryNode with SingleForwarder {
   val sums = new mutable.HashMap[Vector[Any], Any].withDefault(d => 0)
 
   override def onChangeSet(changeSet: ChangeSet): Unit = {
     val oldValues = new mutable.HashMap[Vector[Any], Any]
 
-//    for (tuple <- changeSet.positive;
-//         key = aggregationKeys.map(tuple(_))) {
-//      if (!oldValues.contains(key)) {
-//        oldValues(key) = sums(key)
-//      }
-//      sums(key) = GenericMath.plus(sums(key), tuple(sumKey))
-//    }
-//
-//    for (tuple <- changeSet.negative;
-//         key = aggregationKeys.map(tuple(_))) {
-//      if (!oldValues.contains(key)) {
-//        oldValues(key) = sums(key)
-//      }
-//      sums(key) = GenericMath.minus(sums(key), tuple(sumKey))
-//    }
-//
-//    val positive = new VectorBuilder[TupleType]
-//    val negative = new VectorBuilder[TupleType]
-//    for ((key, oldValue) <- oldValues) {
-//      if (oldValue != 0) {
-//        negative += Map(as -> oldValues(key)) ++ key.zipWithIndex.map(kv => aggregationKeys(kv._2) -> kv._1).toMap
-//      }
-//      if (sums(key) != 0) {
-//        positive += Map(as -> sums(key)) ++ key.zipWithIndex.map(kv => aggregationKeys(kv._2) -> kv._1).toMap
-//      }
-//    }
-//
-//    forward(ChangeSet(positive = positive.result(), negative = negative.result()))
+    for (tuple <- changeSet.positive;
+         key = aggregationKeys.map(tuple(_))) {
+      if (!oldValues.contains(key)) {
+        oldValues(key) = sums(key)
+      }
+      sums(key) = GenericMath.plus(sums(key), tuple(sumKey))
+    }
+
+    for (tuple <- changeSet.negative;
+         key = aggregationKeys.map(tuple(_))) {
+      if (!oldValues.contains(key)) {
+        oldValues(key) = sums(key)
+      }
+      sums(key) = GenericMath.minus(sums(key), tuple(sumKey))
+    }
+
+    val positive = new VectorBuilder[Tuple]
+    val negative = new VectorBuilder[Tuple]
+    for ((key, oldValue) <- oldValues) {
+      if (oldValue != 0) {
+        negative += key :+ oldValues(key)
+      }
+      if (sums(key) != 0) {
+        positive += key :+ sums(key)
+      }
+    }
+
+    forward(ChangeSet(positive = positive.result(), negative = negative.result()))
   }
 }
--- ire/src/test/scala/AggregationNodeTest.scala
+++ ire/src/test/scala/AggregationNodeTest.scala
@@ -1,80 +1,77 @@
-//import akka.actor.{ActorSystem, Props}
-//import akka.testkit.{ImplicitSender, TestActors, TestKit}
-//import hu.bme.mit.ire.datatypes.TupleType
-//import hu.bme.mit.ire.messages.ChangeSet
-//import hu.bme.mit.ire.nodes.unary.{CountNode, SumNode}
-//import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
-//
-//import scala.concurrent.duration.Duration
-//
-//class AggregationNodeTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-//  with WordSpecLike with Matchers with BeforeAndAfterAll {
-//
-//  val odin: TupleType = Map("city" -> "Asgard", "name" -> "Odin", "weapon" -> "Gungnir", "sex" -> "male", "height" -> 1)
-//  val thor: TupleType = Map(
-//    "city" -> "Asgard", "name" -> "Thor", "weapon" -> "Mjölnir", "sex" -> "male", "height" -> 1.1f)
-//  val freya: TupleType =
-//    Map("city" -> "Asgard", "name" -> "Freya", "sex" -> "female", "height" -> 1.1)
-//  val geirröd: TupleType = Map("city" -> "Jötunheimr", "name" -> "Geirröd ", "sex" -> "male", "height" -> 2L)
-//  val ragnar: TupleType = Map(
-//    "city" -> "Midgard", "name" -> "Ragnarr Loðbrók", "sex" -> "male", "height" -> 0.8)
-//
-//  def this() = this(ActorSystem("MySpec"))
-//
-//  override def afterAll {
-//    TestKit.shutdownActorSystem(system)
-//  }
-//
-//  "Count" must {
-//    "count with complex keys" in {
-//
-//      val echoActor = system.actorOf(TestActors.echoActorProps)
-//      val counter = system.actorOf(Props(new CountNode(echoActor ! _, Vector("city", "sex"), "count")))
-//      counter ! ChangeSet(positive = Vector(odin))
-//      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 1))))
-//      counter ! ChangeSet(positive = Vector(thor))
-//      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 2)),
-//        negative = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 1))))
-//      counter ! ChangeSet(negative = Vector(odin))
-//      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 1)),
-//        negative = Vector(Map("sex" -> "male", "city" -> "Asgard", "count" -> 2))))
-//      counter ! ChangeSet(positive = Vector(freya))
-//      expectMsg(ChangeSet(positive = Vector(Map("sex" -> "female", "city" -> "Asgard", "count" -> 1))))
-//      counter ! ChangeSet(negative = Vector(freya))
-//      expectMsg(ChangeSet(negative = Vector(Map("sex" -> "female", "city" -> "Asgard", "count" -> 1))))
-//    }
-//  }
-//
-//  def assertNextChangeset(key: Any, positive: Option[Any] = None, negative: Option[Any] = None): Unit = {
-//    val cs = receiveOne(Duration.Inf).asInstanceOf[ChangeSet]
-//
-//    def assertEquals(actual: Any, expected: Any) {
-//      (actual, expected) match {
-//        case (actual: Double, expected: Double) => actual should be(expected +- 0.01)
-//        case (actual: Float, expected: Float) => actual should be(expected +- 0.01f)
-//        case _ => actual should be(expected)
-//      }
-//    }
-//    if (positive.isDefined) {
-//      assertEquals(cs.positive(0)(key), positive.get)
-//    }
-//
-//    if (negative.isDefined) {
-//      assertEquals(cs.negative(0)(key), negative.get)
-//    }
-//  }
-//
-//  "Sum" must {
-//    "sum with complex keys" in {
-//      val echoActor = system.actorOf(TestActors.echoActorProps)
-//      val counter = system.actorOf(Props(new SumNode(echoActor ! _, Vector("sex"), "height", "sum")))
-//      counter ! ChangeSet(positive = Vector(odin))
-//      assertNextChangeset(key = "sum", positive = Some(1))
-//      counter ! ChangeSet(positive = Vector(thor))
-//      assertNextChangeset(key = "sum", positive = Some(2.1f), negative = Some(1))
-//      counter ! ChangeSet(positive = Vector(ragnar))
-//      assertNextChangeset(key = "sum", positive = Some(2.9), negative = Some(2.1f))
-//    }
-//  }
-//
-//}
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import hu.bme.mit.ire.datatypes.Tuple
+import hu.bme.mit.ire.messages.ChangeSet
+import hu.bme.mit.ire.nodes.unary.{CountNode, SumNode}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.concurrent.duration.Duration
+
+class AggregationNodeTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  val odin: Tuple = Vector("Asgard", "Odin", "Gungnir", "male", 1)
+  val thor: Tuple = Vector("Asgard", "Thor", "Mjölnir", "male", 1.1f)
+  val freya: Tuple = Vector("Asgard", "Freya", "N/A", "female", 1.1)
+  val geirröd: Tuple = Vector("Jötunheimr", "Geirröd ", "N/A", "male", 2L)
+  val ragnar: Tuple = Vector("Midgard", "Ragnarr Loðbrók", "N/A", "male", 0.8)
+
+  def this() = this(ActorSystem("MySpec"))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "Count" should {
+    "count with complex keys" in {
+
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val counter = system.actorOf(Props(new CountNode(echoActor ! _, Vector(3, 0))))
+      counter ! ChangeSet(positive = Vector(odin))
+      expectMsg(ChangeSet(positive = Vector(Vector("male", "Asgard", 1))))
+      counter ! ChangeSet(positive = Vector(thor))
+      expectMsg(ChangeSet(positive = Vector(Vector("male", "Asgard", 2)),
+        negative = Vector(Vector("male", "Asgard", 1))))
+      counter ! ChangeSet(negative = Vector(odin))
+      expectMsg(ChangeSet(positive = Vector(Vector("male", "Asgard", 1)),
+        negative = Vector(Vector("male", "Asgard", 2))))
+      counter ! ChangeSet(positive = Vector(freya))
+      expectMsg(ChangeSet(positive = Vector(Vector("female", "Asgard", 1))))
+      counter ! ChangeSet(negative = Vector(freya))
+      expectMsg(ChangeSet(negative = Vector(Vector("female", "Asgard", 1))))
+    }
+  }
+
+  def assertNextChangeset(key: Int, positive: Option[Any] = None, negative: Option[Any] = None): Unit = {
+    val cs = receiveOne(Duration("1 s")).asInstanceOf[ChangeSet]
+
+    def assertEquals(actual: Any, expected: Any) {
+      (actual, expected) match {
+        case (actual: Double, expected: Double) => actual should be(expected +- 0.01)
+        case (actual: Float, expected: Float) => actual should be(expected +- 0.01f)
+        case _ => actual should be(expected)
+      }
+    }
+    if (positive.isDefined) {
+      assertEquals(cs.positive(0)(key), positive.get)
+    }
+
+    if (negative.isDefined) {
+      assertEquals(cs.negative(0)(key), negative.get)
+    }
+  }
+
+  "Sum" should {
+    "sum with complex keys" in {
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val counter = system.actorOf(Props(new SumNode(echoActor ! _, Vector(3), 4)))
+      counter ! ChangeSet(positive = Vector(odin))
+      assertNextChangeset(key = 1, positive = Some(1))
+      counter ! ChangeSet(positive = Vector(thor))
+      assertNextChangeset(key = 1, positive = Some(2.1f), negative = Some(1))
+      counter ! ChangeSet(positive = Vector(ragnar))
+      assertNextChangeset(key = 1, positive = Some(2.9), negative = Some(2.1f))
+    }
+  }
+
+}
