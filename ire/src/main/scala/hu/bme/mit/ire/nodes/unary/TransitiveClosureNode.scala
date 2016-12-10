d73ba985d879010680be127fb19a64486454b5dd
Jozsef Makai <reyuulol@gmail.com>
2016-12-10 16:05:51 +0100

    TransitiveClosureNode positive with some simple tests

--- ire/src/main/scala/hu/bme/mit/ire/nodes/unary/TransitiveClosureNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/unary/TransitiveClosureNode.scala
@@ -1,15 +1,63 @@
 package hu.bme.mit.ire.nodes.unary
 
 import hu.bme.mit.ire.SingleForwarder
-import hu.bme.mit.ire.datatypes._
+import hu.bme.mit.ire.datatypes.Tuple
+import hu.bme.mit.ire.util.TestUtil._
 import hu.bme.mit.ire.messages.{ChangeSet, ReteMessage}
+
+import scala.collection.immutable.VectorBuilder
+import scala.collection.mutable
 
 class TransitiveClosureNode(override val next: (ReteMessage) => Unit,
                             val src: Int,
                             val trg: Int,
                             val edge: Int
                            ) extends UnaryNode with SingleForwarder {
-  def onChangeSet(changeSet: ChangeSet): Unit = {
 
+  val reachableVertices = new mutable.HashMap[Long, mutable.HashMap[Long, mutable.MutableList[Vector[Long]]]]
+  val reachableFrom = new mutable.HashMap[Long, mutable.HashSet[Long]]
+
+  def onChangeSet(changeSet: ChangeSet): Unit = {
+    forward(ChangeSet(
+      changeSet.positive.flatMap(newPathsWithEdge),
+      Vector()
+    ))
+  }
+
+  private def newPathsWithEdge(inputTuple: Tuple): Vector[Tuple] = {
+    val sourceId = inputTuple(src).asInstanceOf[Number].longValue
+    val targetId = inputTuple(trg).asInstanceOf[Number].longValue
+    val edgeId = inputTuple(edge).asInstanceOf[Number].longValue
+
+    var newPathsBuilder = new VectorBuilder[Tuple]
+
+    for (source <- reachableFrom.getOrElse(sourceId, Set.empty).toStream #::: Stream(sourceId)) {
+      if (!reachableVertices.contains(source)) {
+        reachableVertices(source) = new mutable.HashMap
+      }
+
+      for (target <- reachableVertices.getOrElse(targetId, Map.empty).keys.toStream #::: Stream(targetId)) {
+        if (!reachableFrom.contains(target)) {
+          reachableFrom(target) = new mutable.HashSet
+        }
+        reachableFrom(target) += source
+
+        if (!reachableVertices(source).contains(target)) {
+          reachableVertices(source)(target) = new mutable.MutableList
+        }
+
+        for (sourcePath <- reachableVertices(source).getOrElse(sourceId, mutable.MutableList(Vector()))) {
+          for (targetPath <- reachableVertices.getOrElse(targetId, mutable.HashMap()).getOrElse(target, mutable.MutableList(Vector()))) {
+            val path = sourcePath ++ Vector(edgeId) ++ targetPath
+
+            reachableVertices(source)(target) += path
+
+            newPathsBuilder += tuple(source, target, path)
+          }
+        }
+      }
+    }
+
+    newPathsBuilder.result
   }
 }
--- ire/src/test/scala/TransitiveClosureNodeTest.scala
+++ ire/src/test/scala/TransitiveClosureNodeTest.scala
@@ -1,6 +1,6 @@
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActors, TestKit}
-import hu.bme.mit.ire.messages.{ChangeSet, Primary, Secondary}
+import hu.bme.mit.ire.messages.ChangeSet
 import hu.bme.mit.ire.nodes.unary.TransitiveClosureNode
 import hu.bme.mit.ire.util.TestUtil._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
@@ -17,20 +17,42 @@ class TransitiveClosureNodeTest(_system: ActorSystem) extends TestKit(_system) w
   "A TransitiveClosureNode" must {
     "calculate transitive closure" in {
       val changeSet = ChangeSet(
-        positive = Vector(tuple(1, "a", 2), tuple(2, "b", 3))
+        positive = Vector(tuple(1, 100, 2), tuple(2, 101, 3))
       )
       val src = 0
       val trg = 2
       val edge = 1
       val echoActor = system.actorOf(TestActors.echoActorProps)
-      val transitiveClosure = system.actorOf(Props(new TransitiveClosureNode(echoActor ! _, src, trg, edge)), name = "testSelector")
+      val transitiveClosure = system.actorOf(Props(new TransitiveClosureNode(echoActor ! _, src, trg, edge)))
 
       transitiveClosure ! changeSet
-//      expectMsg(ChangeSet(
-//        positive = Vector(tuple(1, 2), tuple(2, 3), tuple(2, 3))
-//      ))
+      expectMsg(ChangeSet(
+        positive = Vector(tuple(1, 2, Vector(100)), tuple(1, 3, Vector(100, 101)), tuple(2, 3, Vector(101)))
+      ))
     }
   }
 
+  "A TransitiveClosureNode" must {
+    "handle multiple paths and edges between vertices" in {
+      val changeSet = ChangeSet(
+        positive = Vector(tuple(1, 100, 2), tuple(2, 101, 3))
+      )
+      val src = 0
+      val trg = 2
+      val edge = 1
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val transitiveClosure = system.actorOf(Props(new TransitiveClosureNode(echoActor ! _, src, trg, edge)))
+
+      transitiveClosure ! changeSet
+      expectMsg(ChangeSet(
+        positive = Vector(tuple(1, 2, Vector(100)), tuple(1, 3, Vector(100, 101)), tuple(2, 3, Vector(101)))
+      ))
+
+      transitiveClosure ! ChangeSet(positive = Vector(tuple(1, 102, 3), tuple(2, 103, 3)))
+      expectMsg(ChangeSet(
+        positive = Vector(tuple(1, 3, Vector(102)), tuple(1, 3, Vector(100, 103)), tuple(2, 3, Vector(103)))
+      ))
+    }
+  }
 
 }
