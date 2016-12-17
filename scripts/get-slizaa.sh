5f8372e71f7d74de6d1a5877a3fe53ead7db855c
Gabor Szarnyas <szarnyasg@gmail.com>
2016-12-17 16:29:00 -0500

    Depend on a specific (currently latest) version of Slizaa, fix ScalaTest dependency

--- scripts/get-slizaa.sh
+++ scripts/get-slizaa.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+SLIZAA_COMMIT_ID=664a6df2b8c86344d13956fcd27efadde470371f
+
 cd "$( cd "$( dirname "$0" )" && pwd )/../../"
 git clone https://github.com/slizaa/slizaa-opencypher-xtext/
 cd slizaa-opencypher-xtext
+git checkout $SLIZAA_COMMIT_ID
 mvn clean install -q -DskipTests
