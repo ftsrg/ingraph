280479de2656409125cde2451a5fd970af3f4db6
Gabor Szarnyas <szarnyasg@gmail.com>
2016-12-18 17:19:42 -0500

    Add skeleton for left outer join

--- ire/src/main/scala/hu/bme/mit/ire/nodes/binary/LeftOuterJoinNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/binary/LeftOuterJoinNode.scala
@@ -1,5 +1,18 @@
 package hu.bme.mit.ire.nodes.binary
 
-class LeftOuterJoinNode {
+import hu.bme.mit.ire.SingleForwarder
+import hu.bme.mit.ire.datatypes._
+import hu.bme.mit.ire.messages.{ChangeSet, ReteMessage}
+
+class LeftOuterJoinNode(override val next: (ReteMessage) => Unit,
+                        override val primaryMask: Mask,
+                        override val secondaryMask: Mask)
+  extends AbstractJoinNode(primaryMask, secondaryMask) with SingleForwarder {
+
+  def onPrimary(changeSet: ChangeSet): Unit = {
+  }
+
+  def onSecondary(changeSet: ChangeSet): Unit = {
+  }
 
 }
