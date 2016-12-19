a5a5fb6a4a5bd778bcc7956793616d52e3a604af
Gabor Szarnyas <szarnyasg@gmail.com>
2016-12-18 19:05:10 -0500

    Implement MaxNode w/ tests, moved common MinNode/MaxNode logic ExtremeNode

--- ire/src/main/scala/hu/bme/mit/ire/nodes/unary/MinNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/unary/MinNode.scala
@@ -9,46 +9,11 @@ import scala.collection.immutable.VectorBuilder
 import scala.collection.mutable
 
 class MinNode(override val next: (ReteMessage) => Unit,
-        val aggregationKeys: Vector[Int], val minKey: Int,
-        override val expectedTerminatorCount: Int = 1
-           ) extends UnaryNode with SingleForwarder {
-  implicit val order = new Ordering[Tuple] {
-  override def compare(x: Tuple, y: Tuple): Int = GenericMath.compare(y(minKey), x(minKey))
+        override val aggregationKeys: Vector[Int],
+        override val extremeKey: Int,
+        override val expectedTerminatorCount: Int = 1)
+  extends ExtremeNode(next, aggregationKeys, extremeKey, expectedTerminatorCount) {
+  override implicit val order = new Ordering[Tuple] {
+  override def compare(x: Tuple, y: Tuple): Int = GenericMath.compare(y(extremeKey), x(extremeKey))
   }
-  val data = new mutable.HashMap[Vector[Any], mutable.SortedSet[Tuple]]()
-
-  override def onChangeSet(changeSet: ChangeSet): Unit = {
-
-  val oldValues = new mutable.HashMap[Vector[Any], Option[Any]]
-
-  for (tuple <- changeSet.positive;
-     key = aggregationKeys.map(tuple(_))) {
-    if (!oldValues.contains(key)) {
-    oldValues(key) = data.get(key).map(_.last(minKey))
-    }
-    data.getOrElseUpdate(key, mutable.SortedSet[Tuple]()) += tuple
-  }
-
-  for (tuple <- changeSet.negative;
-     key = aggregationKeys.map(tuple(_))) {
-    if (!oldValues.contains(key)) {
-    oldValues(key) = data.get(key).map(_.last(minKey))
-    println(oldValues)
-    }
-    data(key) -= tuple
-  }
-  val positive = new VectorBuilder[Tuple]
-  val negative = new VectorBuilder[Tuple]
-  for ((key, oldValue) <- oldValues) {
-    if (oldValue.isEmpty || data(key) != oldValue.get)
-    if (oldValue.isDefined) {
-    negative += key :+ oldValues(key).get
-    }
-
-    positive += key :+ data(key).last(minKey)
-  }
-
-  forward(ChangeSet(positive = positive.result(), negative = negative.result()))
-  }
-
 }
