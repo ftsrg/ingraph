e6e865a04c59a2fa3fe174a4ccdf5c936f4d128d
Gabor Szarnyas <szarnyasg@gmail.com>
2016-12-27 13:17:35 +0100

    Scripts

--- scripts/clean-build-dirs.sh
+++ scripts/clean-build-dirs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-find . -type d -name build | xargs rm -rf 
+cd "$( cd "$( dirname "$0" )" && pwd )/../"
+find . -type d -name build | xargs rm -rf
 find . -type d -name xtend-gen | xargs rm -rf
