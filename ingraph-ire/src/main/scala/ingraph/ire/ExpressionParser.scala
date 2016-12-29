72eb02d4b59a634c0a90a5d5674cd3b74fb6b935
János Maginecz <wafleeee@gmail.com>
2016-12-29 20:52:26 +0100

    Remove unnecessary pattern match layer

--- ingraph-ire/src/main/scala/ingraph/ire/ExpressionParser.scala
+++ ingraph-ire/src/main/scala/ingraph/ire/ExpressionParser.scala
@@ -6,77 +6,78 @@ import relalg._
 object ExpressionParser {
   def parse(expression: Expression): (TupleType) => Boolean =
     expression match {
-      case exp: UnaryExpression =>
-        exp match {
-          case exp: UnaryLogicalExpression =>
-            val operand: (TupleType => Boolean) = parse(exp.getLeftOperand)
-            import UnaryLogicalOperator._
-            exp.getOperator match {
-              case NOT => (t: TupleType) => !operand(t)
-            }
+      case exp: UnaryLogicalExpression =>
+        val operand: (TupleType => Boolean) = parse(exp.getLeftOperand)
+        import UnaryLogicalOperator._
+        exp.getOperator match {
+          case NOT => (t: TupleType) => !operand(t)
         }
 
-      case exp: BinaryExpression =>
-        exp match {
-          case exp: BinaryLogicalExpression =>
-            val left: (TupleType => Boolean) = parse(exp.getLeftOperand)
-            val right: (TupleType => Boolean) = parse(exp.getRightOperand)
-            import BinaryLogicalOperator._
-            exp.getOperator match {
-              case AND => (t: TupleType) => left(t) && right(t)
-              case OR => (t: TupleType) => left(t) || right(t)
-              case XOR => (t: TupleType) => left(t) ^ right(t)
+      case exp: BinaryLogicalExpression =>
+        val left: (TupleType => Boolean) = parse(exp.getLeftOperand)
+        val right: (TupleType => Boolean) = parse(exp.getRightOperand)
+        import BinaryLogicalOperator._
+        exp.getOperator match {
+          case AND => (t: TupleType) => left(t) && right(t)
+          case OR => (t: TupleType) => left(t) || right(t)
+          case XOR => (t: TupleType) => left(t) ^ right(t)
+        }
+
+      case exp: ArithmeticComparisonExpression =>
+        def left: (TupleType) => Any = parseComparable(exp.getLeftOperand, _)
+
+        def right: (TupleType) => Any = parseComparable(exp.getRightOperand, _)
+        import ArithmeticComparisonOperator._
+        exp.getOperator match {
+          case EQUAL_TO => (t: TupleType) => left(t) == right(t)
+          case NOT_EQUAL_TO => (t: TupleType) => left(t) != right(t)
+          case LESS_THAN_OR_EQUAL => (t: TupleType) =>
+            left(t) match {
+              case l: Double => right(t) match {
+                case r: Double => l <= r
+                case r: Integer => l <= r
+              }
+              case l: Integer => right(t) match {
+                case r: Double => l <= r
+                case r: Integer => l <= r
+              }
             }
-          case exp: ArithmeticComparisonExpression =>
-            def left: (TupleType) => Any = parseComparable(exp.getLeftOperand, _)
-            def right: (TupleType) => Any = parseComparable(exp.getRightOperand, _)
-            import ArithmeticComparisonOperator._
-            exp.getOperator match {
-              case EQUAL_TO => (t: TupleType) => left(t) == right(t)
-              case NOT_EQUAL_TO => (t: TupleType) => left(t) != right(t)
-              case LESS_THAN_OR_EQUAL => (t: TupleType) => left(t) match {
-                case l: Double => right(t) match {
-                  case r: Double => l <= r
-                  case r: Integer => l <= r
-                }
-                case l: Integer => right(t) match {
-                  case r: Double => l <= r
-                  case r: Integer => l <= r
-                }
+          case LESS_THAN => (t: TupleType) =>
+            left(t) match {
+              case l: Double => right(t) match {
+                case r: Double => l < r
+                case r: Integer => l < r
               }
-              case LESS_THAN => (t: TupleType) => left(t) match {
-                case l: Double => right(t) match {
-                  case r: Double => l < r
-                  case r: Integer => l < r
-                }
-                case l: Integer => right(t) match {
-                  case r: Double => l < r
-                  case r: Integer => l < r
-                }
+              case l: Integer => right(t) match {
+                case r: Double => l < r
+                case r: Integer => l < r
               }
-              case GREATER_THAN_OR_EQUAL => (t: TupleType) => left(t) match {
-                case l: Double => right(t) match {
-                  case r: Double => l >= r
-                  case r: Integer => l >= r
-                }
-                case l: Integer => right(t) match {
-                  case r: Double => l >= r
-                  case r: Integer => l >= r
-                }
+            }
+          case GREATER_THAN_OR_EQUAL => (t: TupleType) =>
+            left(t) match {
+              case l: Double => right(t) match {
+                case r: Double => l >= r
+                case r: Integer => l >= r
               }
-              case GREATER_THAN => (t: TupleType) => left(t) match {
-                case l: Double => right(t) match {
-                  case r: Double => l > r
-                  case r: Integer => l > r
-                }
-                case l: Integer => right(t) match {
-                  case r: Double => l > r
-                  case r: Integer => l > r
-                }
+              case l: Integer => right(t) match {
+                case r: Double => l >= r
+                case r: Integer => l >= r
+              }
+            }
+          case GREATER_THAN => (t: TupleType) =>
+            left(t) match {
+              case l: Double => right(t) match {
+                case r: Double => l > r
+                case r: Integer => l > r
+              }
+              case l: Integer => right(t) match {
+                case r: Double => l > r
+                case r: Integer => l > r
               }
             }
         }
     }
+
 
   private def parseComparable(cmp: ComparableExpression, tuple: TupleType) = cmp match {
     case cmp: DoubleLiteral => cmp.getValue
