9eb1e6c350494cec862db0bc4fb0eb7c28841ae2
Gabor Szarnyas <szarnyasg@gmail.com>
2016-12-31 16:24:16 +0100

    Fix projects in build, update Slizaa download script #49

--- scripts/get-slizaa.sh
+++ scripts/get-slizaa.sh
@@ -5,7 +5,8 @@ set -e # exit with nonzero exit code if anything fails
 SLIZAA_COMMIT_ID=664a6df2b8c86344d13956fcd27efadde470371f
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../../"
-git clone https://github.com/slizaa/slizaa-opencypher-xtext/
+git clone https://github.com/slizaa/slizaa-opencypher-xtext/ || true
 cd slizaa-opencypher-xtext
 git checkout $SLIZAA_COMMIT_ID
+echo "Building slizaa-opencypher-xtext in quiet mode (only prints to console for the Xtext generator and in case of errors)"
 mvn clean install -q -DskipTests
