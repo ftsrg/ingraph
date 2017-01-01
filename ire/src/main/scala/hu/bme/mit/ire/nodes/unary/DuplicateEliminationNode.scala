353d239eb548b487e3113493ef67f9cdd7a6abaf
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-01 18:17:34 +0100

    Implement duplicate elimination node & add tests

--- ire/src/main/scala/hu/bme/mit/ire/nodes/unary/DuplicateEliminationNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/unary/DuplicateEliminationNode.scala
@@ -3,11 +3,41 @@ package hu.bme.mit.ire.nodes.unary
 import hu.bme.mit.ire.SingleForwarder
 import hu.bme.mit.ire.datatypes.Tuple
 import hu.bme.mit.ire.messages.{ChangeSet, ReteMessage}
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
-class DuplicateEliminationNode(override val next: (ReteMessage) => Unit,
-                               val condition: (Tuple) => Boolean) extends UnaryNode with SingleForwarder {
+class DuplicateEliminationNode(override val next: (ReteMessage) => Unit) extends UnaryNode with SingleForwarder {
   override def onSizeRequest(): Long = 0
+
+  val tuples = new mutable.HashMap[Tuple, Integer].withDefault(d => 0)
+  
   def onChangeSet(changeSet: ChangeSet): Unit = {
-    // TODO
+    val forwardPositive = new ListBuffer[Tuple]()
+    val forwardNegative = new ListBuffer[Tuple]()
+    
+    changeSet.positive.foreach { tuple =>
+      val oldCount = tuples(tuple)
+      val newCount = oldCount + 1
+      tuples.put(tuple, newCount)
+      
+      if (oldCount == 0) {
+        forwardPositive += tuple
+      }
+    }
+    
+    println(forwardPositive)
+    
+    changeSet.negative.foreach { tuple =>
+      val oldCount = tuples(tuple)
+      val newCount = oldCount - 1
+      tuples.put(tuple, newCount)
+      
+      if (newCount == 0) {
+        forwardNegative += tuple
+      }
+    }
+    
+    forward(ChangeSet(forwardPositive, forwardNegative))
   }
+  
 }
