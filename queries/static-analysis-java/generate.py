f554af7bd4198a2cc350813c3adb342767f6bf6b
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-02 11:05:10 +0100

    Update generate script for Java/static analysis queries

--- queries/static-analysis-java/generate.py
+++ queries/static-analysis-java/generate.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import glob
+import re
 import sys
 import xml.etree.ElementTree as ET
-import glob
 
 for xmlFileName in glob.glob("../../../jqa-java-plugin/src/main/resources/META-INF/jqassistant-rules/*.xml"):
     tree = ET.parse(xmlFileName)
@@ -11,9 +12,10 @@ for xmlFileName in glob.glob("../../../jqa-java-plugin/src/main/resources/META-I
     for child in root:
         queryName = child.attrib['id'].replace(':', '-')
         queryDescription = child.find('description').text
+        queryDescription = re.sub('\s+', ' ', queryDescription)
         cypherString = child.find('cypher').text.replace('            ', '')
         queryFileContent = "// {}\n{}".format(queryDescription, cypherString)
 
-        cypherFileName = "{}.cypher".format(queryName)
-        with open(cypherFileName, "w") as text_file:
+        cypherFileName = '{}.cypher'.format(queryName)
+        with open(cypherFileName, 'w') as text_file:
             text_file.write(queryFileContent)
