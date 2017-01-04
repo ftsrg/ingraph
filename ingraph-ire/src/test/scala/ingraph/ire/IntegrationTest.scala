d83607753fb5f938b9aef90e4096f5ed0719e946
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-04 19:21:27 +0100

    Add tests with larger models

--- ingraph-ire/src/test/scala/ingraph/ire/IntegrationTest.scala
+++ ingraph-ire/src/test/scala/ingraph/ire/IntegrationTest.scala
@@ -6,16 +6,23 @@ import org.scalatest.FlatSpec
 import scala.io.Source
 
 class IntegrationTest extends FlatSpec {
-  val modelPath = "src/test/resources/railway-repair-1-tinkerpop.graphml"
+  
+  def modelPath(size: Int) = s"src/test/resources/railway-repair-$size-tinkerpop.graphml"
   def queryPath(query: String): String = s"../queries/trainbenchmark/$query.cypher"
-  case class TestCase(name: String, expectedResultSize: Int)
+  case class TestCase(name: String, size: Int, expectedResultSize: Int)
 
   Vector(
-    TestCase("PosLength", 95),
-    TestCase("RouteSensor", 18),
-    TestCase("SemaphoreNeighbor", 3),
-    TestCase("SwitchMonitored", 0),
-    TestCase("SwitchSet", 5)
+    TestCase("PosLength",         1, 95),
+    TestCase("RouteSensor",       1, 18),
+    TestCase("SemaphoreNeighbor", 1, 3),
+    TestCase("SwitchMonitored",   1, 0),
+    TestCase("SwitchSet",         1, 5),
+    //
+    TestCase("PosLength",         2, 208), 
+    TestCase("RouteSensor",       2, 33),
+    TestCase("SemaphoreNeighbor", 2, 6),
+    TestCase("SwitchMonitored",   2, 2),
+    TestCase("SwitchSet",         2, 8)    
   ).foreach(
     t => t.name should "work" in {
       val query = Source.fromFile(queryPath(t.name)).getLines().mkString
@@ -24,7 +31,7 @@ class IntegrationTest extends FlatSpec {
       tf.subscribe(adapter.engine.inputLookup)
       val tran = tf.newBatchTransaction()
 
-      adapter.readGraph(modelPath, tran)
+      adapter.readGraph(modelPath(t.size), tran)
       tran.close()
       val results = adapter.engine.getResults().size
       assert(results == t.expectedResultSize)
