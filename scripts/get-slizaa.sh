d895dc26e395895f9ff52875ef3c3cfa31884c14
wafle <wafle@users.noreply.github.com>
2017-01-04 20:04:55 +0100

    Fetch updates before checkout

--- scripts/get-slizaa.sh
+++ scripts/get-slizaa.sh
@@ -7,6 +7,7 @@ SLIZAA_COMMIT_ID=ab5b7e8b6e73e437a7edddf961cc6a9aa990a0fb
 cd "$( cd "$( dirname "$0" )" && pwd )/../../"
 git clone https://github.com/slizaa/slizaa-opencypher-xtext/ || true
 cd slizaa-opencypher-xtext
+git fetch
 git checkout $SLIZAA_COMMIT_ID
 echo "Building slizaa-opencypher-xtext in quiet mode (only prints to console for the Xtext generator and in case of errors)"
 mvn clean install -q -DskipTests
