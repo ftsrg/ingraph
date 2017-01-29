9d1485f03c5065f35bd0eb11dc5eac0427240d77
János Maginecz <wafleeee@gmail.com>
2017-01-29 17:05:31 +0100

    Add connectedsegments to integrationtest

--- ingraph-ire/src/test/scala/ingraph/ire/IntegrationTest.scala
+++ ingraph-ire/src/test/scala/ingraph/ire/IntegrationTest.scala
@@ -17,12 +17,14 @@ class IntegrationTest extends FlatSpec {
     TestCase("SemaphoreNeighbor", 1, 3),
     TestCase("SwitchMonitored",   1, 0),
     TestCase("SwitchSet",         1, 5),
+    TestCase("ConnectedSegments", 1, 8),
     //
     TestCase("PosLength",         2, 208),
     TestCase("RouteSensor",       2, 33),
     TestCase("SemaphoreNeighbor", 2, 6),
     TestCase("SwitchMonitored",   2, 2),
-    TestCase("SwitchSet",         2, 8)
+    TestCase("SwitchSet",         2, 8),
+    TestCase("ConnectedSegments", 2, 16)
   ).foreach(
     t => s"${t.name}-size-${t.size}" should "work" in {
       val query = Source.fromFile(queryPath(t.name)).getLines().mkString(" ")
