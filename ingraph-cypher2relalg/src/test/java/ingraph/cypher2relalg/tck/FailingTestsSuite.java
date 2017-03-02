0bf39a9c342ed86dff38903d83ae45b5c8bea761
Jozsef Marton <marton@db.bme.hu>
2017-03-02 23:44:06 +0100

    Cleanup test suite descriptors

--- ingraph-cypher2relalg/src/test/java/ingraph/cypher2relalg/tck/FailingTestsSuite.java
+++
@@ -1,8 +0,0 @@
-package ingraph.cypher2relalg.tck;
-
-import org.junit.experimental.categories.Categories;
-
-@Categories.IncludeCategory(FailingTests.class)
-public class FailingTestsSuite extends IngraphParserTestSuite {
-
-}
--- ingraph-cypher2relalg/src/test/java/ingraph/cypher2relalg/tck/RegressionTestsSuite.java
+++
@@ -1,8 +0,0 @@
-package ingraph.cypher2relalg.tck;
-
-import org.junit.experimental.categories.Categories;
-
-@Categories.IncludeCategory(RegressionTests.class)
-public class RegressionTestsSuite extends IngraphParserTestSuite {
-
-}
