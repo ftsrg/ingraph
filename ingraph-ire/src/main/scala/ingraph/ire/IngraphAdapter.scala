54e4c7a11df8ce47cc0d4b3da5115e4eb6198264
Gabor Szarnyas <szarnyasg@gmail.com>
2017-03-18 12:11:35 +0100

    Fix import

--- ingraph-ire/src/main/scala/ingraph/ire/IngraphAdapter.scala
+++ ingraph-ire/src/main/scala/ingraph/ire/IngraphAdapter.scala
@@ -3,12 +3,12 @@ package ingraph.ire
 import org.apache.tinkerpop.gremlin.structure.Graph
 import org.apache.tinkerpop.gremlin.structure.io.IoCore
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph
+
 import com.tinkerpop.blueprints.util.wrappers.event.EventGraph
+
 import hu.bme.mit.ire.Transaction
 import ingraph.cypher2relalg.Cypher2Relalg
-import relalg.RelalgContainer
-import ingraph.relalg.inferencers.OneStepSchemaInferencer
-import ingraph.optimization.transformations.relalg2rete.Relalg2ReteTransformationAndInferencer
+import ingraph.relalg2rete.Relalg2ReteTransformationAndInferencer
 
 class IngraphAdapter(plan: String) {
   private val retePlan = Relalg2ReteTransformationAndInferencer.apply(Cypher2Relalg.processString(plan))
