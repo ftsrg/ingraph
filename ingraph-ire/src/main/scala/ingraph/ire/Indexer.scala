9a1825e9c9821b0ab2ee7e6ed7f7793cc8d74af9
Gabor Szarnyas <szarnyasg@gmail.com>
2017-03-19 17:19:54 +0100

    Make indexer classes referrable and unwrap neo4j variables

--- ingraph-ire/src/main/scala/ingraph/ire/Indexer.scala
+++ ingraph-ire/src/main/scala/ingraph/ire/Indexer.scala
@@ -1,40 +1,46 @@
 package ingraph.ire
 
 import hu.bme.mit.ire.util.BufferMultimap
-import org.neo4j.driver.internal.{InternalNode, InternalRelationship}
+
 import org.neo4j.driver.v1.Value
+import org.neo4j.driver.v1.types.{Node, Relationship}
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable
+
+
+class IngraphVertex(node: Node, indexer: Indexer) {
+  val id: Long = node.id()
+  val properties: Map[String, AnyRef]  = node.asMap().toMap
+  val edges: mutable.ListMap[String, IngraphEdge] = mutable.ListMap[String, IngraphEdge]()
+  val reverseEdges: mutable.ListMap[String, IngraphEdge] = mutable.ListMap[String, IngraphEdge]()
+  val labels = node.labels().toSet
+  for (label <- labels)
+    indexer.vertexLabelLookup.addBinding(label, this)
+  indexer.nodeLookup(id) = this
+
+  override def toString: String = s"Vertex($id, $properties)"
+}
+
+class IngraphEdge(relation: Relationship, indexer: Indexer) {
+  val id: Long = relation.id()
+  val properties: Map[String, AnyRef] = relation.asMap().toMap
+  val sourceVertex: IngraphVertex = indexer.nodeLookup(relation.startNodeId())
+  sourceVertex.edges(relation.`type`()) = this
+  val targetVertex: IngraphVertex = indexer.nodeLookup(relation.endNodeId())
+  targetVertex.reverseEdges(relation.`type`()) = this
+  indexer.edgeLabelLookup.addBinding(relation.`type`(), this)
+  override def toString: String = s"Edge(${sourceVertex.id} -[$id]-> ${targetVertex.id}, $properties)"
+}
+
 class Indexer {
 
-  private val nodeLookup = mutable.HashMap[Long, IngraphVertex]()
-  private val vertexLabelLookup = new BufferMultimap[String, IngraphVertex]()
-  private val edgeLabelLookup = new BufferMultimap[String, IngraphEdge]()
+  val nodeLookup = mutable.HashMap[Long, IngraphVertex]()
+  val vertexLabelLookup = new BufferMultimap[String, IngraphVertex]()
+  val edgeLabelLookup = new BufferMultimap[String, IngraphEdge]()
 
-  def addVertex(node: InternalNode) = new IngraphVertex(node)
-  def addEdge(relation: InternalRelationship) = new IngraphEdge(relation)
-
-  class IngraphVertex(node: InternalNode) {
-    val id: Long = node.id()
-    val properties: Map[String, Value] = node.keys().map(k => k -> node.get(k)).toMap
-    val edges: mutable.ListMap[String, IngraphEdge] = mutable.ListMap[String, IngraphEdge]()
-    val reverseEdges: mutable.ListMap[String, IngraphEdge] = mutable.ListMap[String, IngraphEdge]()
-    for (label <- node.labels())
-      vertexLabelLookup.addBinding(label, this)
-    nodeLookup(id) = this
-  }
-
-
-  class IngraphEdge(relation: InternalRelationship) {
-    val id: Long = relation.id()
-    val properties: Map[String, Value] = relation.keys().map(k => k -> relation.get(k)).toMap
-    val inVertex: IngraphVertex = nodeLookup(relation.startNodeId())
-    inVertex.edges(relation.`type`()) = this
-    val outVertex: IngraphVertex = nodeLookup(relation.endNodeId())
-    outVertex.reverseEdges(relation.`type`()) = this
-    edgeLabelLookup.addBinding(relation.`type`(), this)
-  }
+  def addVertex(node: Node) = new IngraphVertex(node, this)
+  def addEdge(relation: Relationship) = new IngraphEdge(relation, this)
 
   def vertexById(id: Long): IngraphVertex = nodeLookup(id)
   def verticesByLabel(label: String): Seq[IngraphVertex] = vertexLabelLookup(label)
