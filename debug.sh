c08d8538d468f90ad42ec555566a0f98d805fa83
Gabor Szarnyas <szarnyasg@gmail.com>
2017-04-11 13:06:28 +0200

    Refine debug conf

--- debug.sh
+++ debug.sh
@@ -2,15 +2,18 @@
 
 while true; do
   cd ~/git/ingraph
-  gradle clean distzip && cd ingraph-optimization-tests/build/distributions/ && unzip ingraph-optimization-tests-0.2.0.zip
+  gradle clean distzip
+  cd ingraph-optimization-tests/build/distributions/
+  unzip ingraph-optimization-tests-0.2.0.zip
 
   cd ingraph-optimization-tests-0.2.0
 
-  for i in `seq 0 9`; do
+  for i in `seq 0 29`; do
     echo $i
     bin/ingraph-optimization-tests > /dev/null 2> output.log
     if ! grep sortAndTopOperator output.log; then
       echo error found
+      cat output.log
       exit
     fi
   done
