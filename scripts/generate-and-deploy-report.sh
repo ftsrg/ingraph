f670c3a15017baffeef3db42fb724fbd0a4b005d
Gabor Szarnyas <szarnyasg@gmail.com>
2017-04-11 17:01:34 +0200

    Extracted report generation & deployment to a separate script

---
+++ scripts/generate-and-deploy-report.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# you need to set the GH_TOKEN and GH_REF environment variables for the `deploy.sh` script to work
+
+set -e
+
+./gradlew report --stacktrace --quiet
+cd opencypher-report
+make
+cd ..
+scripts/deploy.sh
