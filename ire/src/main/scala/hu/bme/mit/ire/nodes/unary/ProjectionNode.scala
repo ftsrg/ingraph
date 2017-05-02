d3cb34c1bbbae4bdac05972843167e65d8482eab
János Maginecz <wafleeee@gmail.com>
2017-05-02 17:18:35 +0200

    Start new grouping implementation

--- ire/src/main/scala/hu/bme/mit/ire/nodes/unary/ProjectionNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/unary/ProjectionNode.scala
@@ -1,19 +1,19 @@
 package hu.bme.mit.ire.nodes.unary
 
 import hu.bme.mit.ire.SingleForwarder
-import hu.bme.mit.ire.datatypes.Mask
+import hu.bme.mit.ire.datatypes.{Mask, Tuple}
 import hu.bme.mit.ire.messages.{ChangeSet, ReteMessage}
 
-abstract class ProjectionImpl(val mask: Mask) extends UnaryNode {
+abstract class ProjectionImpl(val mask: Vector[Tuple => Any]) extends UnaryNode {
   override def onSizeRequest() = 0
   override def onChangeSet(changeSet: ChangeSet) = {
     forward(ChangeSet(
-      changeSet.positive.map(t => mask.map(i => t(i))),
-      changeSet.negative.map(t => mask.map(i => t(i)))
+      changeSet.positive.map(t => mask.map(f => f(t))),
+      changeSet.negative.map(t => mask.map(f => f(t)))
     ))
   }
 }
 
 class ProjectionNode(override val next: (ReteMessage) => Unit,
-                     override val mask: Mask)
+                     override val mask: Vector[Tuple => Any])
   extends ProjectionImpl(mask) with SingleForwarder {}
