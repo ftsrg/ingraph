28f26345b20b020d9bc986a10d0dfce3a9fb059d
János Maginecz <wafleeee@gmail.com>
2017-05-09 19:32:49 +0200

    Reclaim random test

--- ingraph-ire/src/test/scala/ingraph/ire/RandomTest.scala
+++ ingraph-ire/src/test/scala/ingraph/ire/RandomTest.scala
@@ -4,11 +4,16 @@ import org.scalatest.FunSuite
 
 import scala.io.Source
 
-class DualTest extends FunSuite {
+class RandomTest extends FunSuite {
   test("Optional match returns Vector(null) on empty input") {
     val query = "OPTIONAL MATCH (n) RETURN n"
     val adapter = new IngraphAdapter(query, "opt")
     assert(adapter.engine.getResults() == List(Vector(null)))
   }
 
+  test("Constant returns work") {
+    val query = "RETURN 1 + 1"
+    val adapter = new IngraphAdapter(query, "opt")
+    assert(adapter.engine.getResults() == List(Vector(2 )))
+  }
 }
