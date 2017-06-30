36258d0847342462c0a9298416e2cc8e2c1d844a
Gabor Szarnyas <szarnyasg@gmail.com>
2017-07-01 00:04:29 +0200

    Use texfot for make

--- scripts/travis-tex-run-required.sh
+++ scripts/travis-tex-run-required.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git show --name-only | grep "ingraph-compiler/"
+git show --name-only | grep "(ingraph-compiler|opencypher-report)/"
 [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a $? -ne 0 ]
