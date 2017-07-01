6a2b01713e3459f39cb5da6da7644be8a8500149
Gabor Szarnyas <szarnyasg@gmail.com>
2017-07-01 09:05:31 +0200

    Fix configuration for recompiling the tech report

--- scripts/travis-tex-run-required.sh
+++ scripts/travis-tex-run-required.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+ 
+# 'grep' returns with 0 if there was a match
 git show --name-only | grep "(ingraph-compiler|opencypher-report)/"
-[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a $? -ne 0 ]
+# we only build on Travis if we are on the master branch, it is not a pull request and the compiler/report changed
+[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a $? = 0 ]
