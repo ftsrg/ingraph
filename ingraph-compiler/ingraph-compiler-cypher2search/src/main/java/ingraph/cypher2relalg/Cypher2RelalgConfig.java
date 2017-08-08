3bd9d349df3ce0223288cc3f1112a04a7ab33c1e
Gabor Szarnyas <szarnyasg@gmail.com>
2017-08-08 13:33:09 +0200

    Remove code for cypher2search project

--- ingraph-compiler/ingraph-compiler-cypher2search/src/main/java/ingraph/cypher2relalg/Cypher2RelalgConfig.java
+++
@@ -1,8 +0,0 @@
-package ingraph.cypher2relalg;
-
-@SuppressWarnings("all")
-public class Cypher2RelalgConfig {
-  public static boolean isDebugMode() {
-    return (("1".equals(System.getenv("DEBUG")) || "1".equals(System.getenv("DEBUG_INGRAPH"))) || "1".equals(System.getenv("DEBUG_INGRAPH_CYPHER2SEARCH")));
-  }
-}
