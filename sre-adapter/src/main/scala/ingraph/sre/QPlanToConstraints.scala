99f03b59da2eeba29e9053845200b59e21a81365
Gabor Szarnyas <szarnyasg@gmail.com>
2017-08-22 10:15:25 +0200

    Add project for converting to constraints

---
+++ sre-adapter/src/main/scala/ingraph/sre/QPlanToConstraints.scala
@@ -0,0 +1,26 @@
+package ingraph.sre
+
+import ingraph.model.iplan.INode
+import ingraph.model.qplan.QNode
+import ingraph.model.{iplan, qplan}
+
+object QPlanToConstraints {
+  def transform(plan: QNode): INode = {
+    plan match {
+      // leaf
+      case qplan.GetVertices(v) => iplan.GetVertices(v)
+
+      // unary
+      case qplan.Expand(src, trg, edge, dir, qplan.GetVertices(v)) => iplan.GetEdges(src, trg, edge, dir)
+      case qplan.Expand(src, trg, edge, dir, child) => iplan.Join(transform(child), iplan.GetEdges(src, trg, edge, dir))
+      case qplan.Top(skipExpr, limitExpr, qplan.Sort(order, child)) => iplan.SortAndTop(skipExpr, limitExpr, order, transform(child))
+      case qplan.Projection(projectList, child) => iplan.Projection(projectList, transform(child))
+      case qplan.Selection(condition, child) => iplan.Selection(condition, transform(child))
+      case qplan.DuplicateElimination(child) => iplan.DuplicateElimination(transform(child))
+      case qplan.AllDifferent(child, edges) => iplan.AllDifferent(transform(child), edges)
+
+      // binary
+      case qplan.Union(l, r) => iplan.Union(transform(l), transform(r))
+    }
+  }
+}
