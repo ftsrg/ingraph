a05f6f78161107352142927eca5f2c1f469f2bbb
Gabor Szarnyas <szarnyasg@gmail.com>
2017-08-27 16:29:17 +0200

    Add nre and nre-adapter projects

--- compiler/src/main/scala/IPlanParser.scala
+++ compiler/src/main/scala/IPlanParser.scala
@@ -1,5 +1,5 @@
 package iplan
-import ingraph.compiler.cypher2qplan
+import ingraph.compiler.CypherToQPlan
 import ingraph.compiler.cypher2qplan.CypherParser
 import ingraph.compiler.qplan2iplan.{QPlanToIPlan, SchemaInferencer}
 import ingraph.model.iplan.INode
@@ -8,7 +8,7 @@ object IPlanParser {
   def parse(query: String): INode = {
     SchemaInferencer.transform(
       QPlanToIPlan.transform(
-        cypher2qplan.build(
+        CypherToQPlan.build(
           CypherParser.parseString(query), "test"
         )
       )
