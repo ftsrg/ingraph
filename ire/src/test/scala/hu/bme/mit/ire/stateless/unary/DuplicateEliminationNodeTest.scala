c9d5b059ddb7a2637f994e1b868647ab761e5cdd
Gabor Szarnyas <szarnyasg@gmail.com>
2017-09-09 21:08:44 +0200

    Add stateless DuplicateEliminationNode #212

---
+++ ire/src/test/scala/hu/bme/mit/ire/stateless/unary/DuplicateEliminationNodeTest.scala
@@ -0,0 +1,50 @@
+package hu.bme.mit.ire.stateless.unary
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import hu.bme.mit.ire.messages.ChangeSet
+import hu.bme.mit.ire.util.TestUtil.{tuple, tupleBag}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class DuplicateEliminationNodeTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("MySpec"))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "DuplicateElimination node" must {
+    "do simple duplicate elimination 0" in {
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val duplicateElimination = system.actorOf(Props(new DuplicateEliminationNode(echoActor ! _)))
+
+      duplicateElimination ! ChangeSet(
+        positive = tupleBag(tuple(1, 2))
+      )
+      expectMsg(ChangeSet(
+        positive = tupleBag(tuple(1, 2))
+      ))
+
+      duplicateElimination ! ChangeSet(
+        positive = tupleBag(tuple(1, 2), tuple(3, 4))
+      )
+      expectMsg(ChangeSet(
+        positive = tupleBag(tuple(1, 2), tuple(3, 4))
+      ))
+    }
+
+    "do simple duplicate elimination 1" in {
+      val echoActor = system.actorOf(TestActors.echoActorProps)
+      val duplicateElimination = system.actorOf(Props(new DuplicateEliminationNode(echoActor ! _)))
+
+      duplicateElimination ! ChangeSet(positive = tupleBag(tuple(1)))
+      expectMsg(ChangeSet(positive = tupleBag(tuple(1))))
+
+      duplicateElimination ! ChangeSet(positive = tupleBag(tuple(1), tuple(1)))
+      expectMsg(ChangeSet(positive = tupleBag(tuple(1))))
+    }
+
+  }
+}
