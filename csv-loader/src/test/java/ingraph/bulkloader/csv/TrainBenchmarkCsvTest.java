094df76571a0ef07340f1387c70b9cd534318879
Gabor Szarnyas <szarnyasg@gmail.com>
2017-10-13 00:52:36 -0400

    Add (ignored) tests for the Train Benchmark

--- csv-loader/src/test/java/ingraph/bulkloader/csv/TrainBenchmarkCsvTest.java
+++ csv-loader/src/test/java/ingraph/bulkloader/csv/TrainBenchmarkCsvTest.java
@@ -1,44 +1,42 @@
 package ingraph.bulkloader.csv;
 
+import com.google.common.collect.ImmutableMap;
+import ingraph.bulkloader.csv.loader.MassCsvLoader;
+import neo4j.driver.util.GraphPrettyPrinter;
+import org.junit.Test;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Relationship;
+import org.supercsv.prefs.CsvPreference;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
-import org.neo4j.driver.v1.types.Node;
-import org.neo4j.driver.v1.types.Relationship;
-import org.supercsv.prefs.CsvPreference;
-
-import com.google.common.collect.ImmutableMap;
-
-import ingraph.bulkloader.csv.loader.MassCsvLoader;
-import neo4j.driver.util.GraphPrettyPrinter;
-
-public class TrainBenchmarkTest {
+public class TrainBenchmarkCsvTest {
 
 	private static final String PREFIX = TestConstants.GRAPHS_DIR + "trainbenchmark/railway-repair-1-";
 	private static final String POSTFIX = ".csv";
 
 	final Map<String, Collection<String>> nodeFilenames = ImmutableMap.<String, Collection<String>>builder() //
-			.put(PREFIX + "Region" + POSTFIX, Arrays.asList("Region")) //
-			.put(PREFIX + "Route" + POSTFIX, Arrays.asList("Route")) //
-			.put(PREFIX + "Segment" + POSTFIX, Arrays.asList("Segment", "TrackElement")) //
-			.put(PREFIX + "Semaphore" + POSTFIX, Arrays.asList("Semaphore")) //
-			.put(PREFIX + "Sensor" + POSTFIX, Arrays.asList("Sensor")) //
-			.put(PREFIX + "Switch" + POSTFIX, Arrays.asList("Switch", "TrackElement")) //
+			.put(PREFIX + "Region"         + POSTFIX, Arrays.asList("Region")) //
+			.put(PREFIX + "Route"          + POSTFIX, Arrays.asList("Route")) //
+			.put(PREFIX + "Segment"        + POSTFIX, Arrays.asList("Segment", "TrackElement")) //
+			.put(PREFIX + "Semaphore"      + POSTFIX, Arrays.asList("Semaphore")) //
+			.put(PREFIX + "Sensor"         + POSTFIX, Arrays.asList("Sensor")) //
+			.put(PREFIX + "Switch"         + POSTFIX, Arrays.asList("Switch", "TrackElement")) //
 			.put(PREFIX + "SwitchPosition" + POSTFIX, Arrays.asList("SwitchPosition")) //
 			.build();
 
 	final Map<String, String> relationshipFilenames = ImmutableMap.<String, String>builder() //
-			.put(PREFIX + "connectsTo" + POSTFIX, "connectsTo") //
-			.put(PREFIX + "entry" + POSTFIX, "entry") //
-			.put(PREFIX + "exit" + POSTFIX, "exit") //
-			.put(PREFIX + "follows" + POSTFIX, "follows") //
+			.put(PREFIX + "connectsTo"  + POSTFIX, "connectsTo") //
+			.put(PREFIX + "entry"       + POSTFIX, "entry") //
+			.put(PREFIX + "exit"        + POSTFIX, "exit") //
+			.put(PREFIX + "follows"     + POSTFIX, "follows") //
 			.put(PREFIX + "monitoredBy" + POSTFIX, "monitoredBy") //
-			.put(PREFIX + "requires" + POSTFIX, "requires") //
-			.put(PREFIX + "target" + POSTFIX, "target") //
+			.put(PREFIX + "requires"    + POSTFIX, "requires") //
+			.put(PREFIX + "target"      + POSTFIX, "target") //
 			.build();
 
 	@Test
