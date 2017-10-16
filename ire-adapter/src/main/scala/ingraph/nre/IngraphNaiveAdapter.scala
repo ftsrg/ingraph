c56f89b8581a4cbff1517a6f268477d644f4d1ad
Bálint Hegyi <hegyi.balint.b@gmail.com>
2017-10-16 14:00:16 +0200

    Add NRE EngineFactory and Adapter class

---
+++ ire-adapter/src/main/scala/ingraph/nre/IngraphNaiveAdapter.scala
@@ -0,0 +1,46 @@
+package ingraph.nre
+
+import hu.bme.mit.ire.datatypes.Tuple
+import hu.bme.mit.ire.listeners.ChangeListener
+import hu.bme.mit.ire.{Neo4jEntityToTupleMapper, Transaction, TransactionFactory}
+import ingraph.compiler.IPlanParser
+import ingraph.ire.{AbstractIngraphAdapter, EngineFactory, Indexer, LongIdParser}
+
+class IngraphNaiveAdapter(
+    override val querySpecification: String,
+    override val queryName: String,
+    override val indexer: Indexer = new Indexer()
+  ) extends AbstractIngraphAdapter {
+
+
+  override val plan = IPlanParser.parse(querySpecification)
+  override val engine = EngineFactory.createQueryEngine(plan, indexer)
+
+  val transactionFactory = new TransactionFactory(16)
+  transactionFactory.subscribe(engine.inputLookup)
+
+  override val tupleMapper = new Neo4jEntityToTupleMapper(
+    engine.vertexConverters.map(kv => kv._1.toSet -> kv._2.toSet).toMap,
+    engine.edgeConverters.map(kv => kv._1 -> kv._2.toSet).toMap,
+    engine.inputLookup) with LongIdParser
+
+  tupleMapper.transaction = transactionFactory.newBatchTransaction()
+  indexer.subscribe(tupleMapper)
+
+
+  def newTransaction(): Transaction = {
+    val tran = transactionFactory.newBatchTransaction()
+    tupleMapper.transaction = tran
+    tran
+  }
+
+  def result(): Iterable[Tuple] = {
+    tupleMapper.transaction.close()
+    engine.getResults()
+  }
+
+  def addListener(listener: ChangeListener): Unit = {
+    engine.addListener(listener)
+  }
+
+}
