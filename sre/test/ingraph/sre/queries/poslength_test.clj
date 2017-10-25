f8408392e9fbfde5c28321da55e2b8d95840928b
Dávid Szakállas <david.szakallas@gmail.com>
2017-10-25 02:32:32 +0200

    Add tests and profiling

--- sre/test/ingraph/sre/queries/poslength_test.clj
+++ sre/test/ingraph/sre/queries/poslength_test.clj
@@ -2,9 +2,8 @@
   (:require [clojure
              [pprint :as pprint]
              [test :refer :all]]
-            [ingraph.sre.ingraph-proto-1 :refer :all]
             [ingraph.sre.queries
-             [poslength-pattern :refer [p]]
+             [poslength-pattern :as pl]
              [utils :as utils]]
             [ingraph.sre.queries.utils.tb-loader :as tb-loader]
             [sre.plan.pattern :as pattern]
@@ -13,32 +12,46 @@
            [scala.collection.immutable HashMap HashSet]
            scala.Tuple2))
 
-(def poslength-query (pattern/compile p Ingraph {:k 5}))
+(tufte/add-basic-println-handler! {})
+
+(defn compile-pattern [def-sym k]
+  (let [def @(resolve def-sym)]
+    (tufte/profile {} (tufte/p (-> def-sym str keyword) (pattern/compile (:pattern def) (:config def) {:k k})))))
+
+
+(def pl-proto-1 (compile-pattern 'pl/p-1 5))
 
 (comment
-  "eval this to get the tasks -->" (pprint/pprint (utils/get-tasks poslength-query)))
+  "eval this to get the tasks -->" (pprint/pprint (utils/get-tasks pl-proto-1)))
 
+(def pl-proto-2 (compile-pattern 'pl/p-2 5))
+
+(comment
+  "eval this to get the tasks -->" (pprint/pprint (utils/get-tasks pl-proto-2)))
 
 (deftest test-poslength-smoke
   (testing "PosLength query should return 1 row"
-    (tufte/profile {}
-                   (let [smoke (doto (Indexer.)
-                                 (.addVertex (IngraphVertex. 0
-                                                             (-> (HashSet.) (.$plus "Segment"))
-                                                             (-> (HashMap.) (.$plus (Tuple2. "length" -1)))))
-                                 (.addVertex (IngraphVertex. 1
-                                                             (-> (HashSet.) (.$plus "Segment"))
-                                                             (-> (HashMap.) (.$plus (Tuple2. "length" 9))))))]
-                     (is (= (count (tufte/p :poslength-smoke (into [] (pattern/run poslength-query [:segment :length] {:indexer smoke}))))
-                            1))))))
+    (for [proto ['pl-proto-1 'pl-proto-2]]
+      (tufte/profile {}
+                     (let [smoke (doto (Indexer.)
+                                   (.addVertex (IngraphVertex. 0
+                                                               (-> (HashSet.) (.$plus "Segment"))
+                                                               (-> (HashMap.) (.$plus (Tuple2. "length" -1)))))
+                                   (.addVertex (IngraphVertex. 1
+                                                               (-> (HashSet.) (.$plus "Segment"))
+                                                               (-> (HashMap.) (.$plus (Tuple2. "length" 9))))))]
+                       (is (= (count (tufte/p (keyword (str 'smoke '- proto))
+                                              (into [] (pattern/run (-> proto resolve deref) [:segment :length] {:indexer smoke}))))
+                              1)))))))
 
 (def tb-inject-1 (tb-loader/load 'inject-1))
 
-(deftest test-poslenght-tb-inject-1
+(deftest test-poslength-tb-inject-1
   (testing "PosLength query should return expected results for TrainBenchmark inject-1"
     (tufte/profile
      {}
-     (let [inject-1 (tb-loader/load 'inject-1)
-           results (tufte/p :poslength-tb-inject-1
-                            (into [] (pattern/run poslength-query [:segment :length] {:indexer inject-1})))]
-       (is (= (count results) 12))))))
+     (for [proto ['pl-proto-1 'pl-proto-2]]
+       (let [inject-1 (tb-loader/load 'inject-1)
+             results (tufte/p (keyword (str 'rb-inject-1 '- proto))
+                              (into [] (pattern/run (-> proto resolve deref) [:segment :length] {:indexer inject-1})))]
+         (is (= (count results) 12)))))))
