9a20677f1dd60a6ca1f3a972f50722c61e199e9a
Dávid Szakállas <david.szakallas@gmail.com>
2017-10-25 02:32:32 +0200

    Add tests and profiling

--- sre/test/ingraph/sre/queries/poslength_pattern.clj
+++ sre/test/ingraph/sre/queries/poslength_pattern.clj
@@ -1,14 +1,27 @@
 (ns ingraph.sre.queries.poslength-pattern
-  (:require [ingraph.sre.ingraph-proto-1 :refer :all]
+  (:require [ingraph.sre
+             [ingraph-proto-1 :as proto-1]
+             [ingraph-proto-2 :as proto-2]]
             [sre.core :refer :all]
             [sre.plan.pattern :as pattern]))
 
-(def p (pattern/make-pattern [:segment :length] []
-                             [(bind Vertex [:segment])
-                              (bind HasLabels [:segment :-segment-label])
-                              (bind Constant [:-segment-label "Segment"])
-                              (bind Property [:segment :-length-key :length])
-                              (bind Constant [:-length-key "length"])
-                              (bind GenBinaryAssertion [:length :-0 :-<=])
-                              (bind Constant [:-0 0])
-                              (bind Constant [:-<= <=])]))
+(def p-1 {:config proto-1/Ingraph
+          :pattern (pattern/make-pattern [:segment :length] []
+                                         [(bind proto-1/Vertex [:segment])
+                                          (bind proto-1/HasLabels [:segment :-segment-label])
+                                          (bind proto-1/Constant [:-segment-label "Segment"])
+                                          (bind proto-1/Property [:segment :-length-key :length])
+                                          (bind proto-1/Constant [:-length-key "length"])
+                                          (bind proto-1/GenBinaryAssertion [:length :-0 :-<=])
+                                          (bind proto-1/Constant [:-0 0])
+                                          (bind proto-1/Constant [:-<= <=])])})
+(def p-2 {:config proto-2/Ingraph
+          :pattern (pattern/make-pattern [:segment :length] []
+                                         [(bind proto-2/Vertex [:segment])
+                                          (bind proto-2/HasLabels [:segment :-segment-label])
+                                          (bind proto-2/Constant [:-segment-label "Segment"])
+                                          (bind proto-2/Property [:segment :-length-key :length])
+                                          (bind proto-2/Constant [:-length-key "length"])
+                                          (bind proto-2/GenBinaryAssertion [:length :-0 :-<=])
+                                          (bind proto-2/Constant [:-0 0])
+                                          (bind proto-2/Constant [:-<= <=])])})
--- sre/test/ingraph/sre/queries/utils.clj
+++ sre/test/ingraph/sre/queries/utils.clj
@@ -1,3 +1,9 @@
-(ns ingraph.sre.queries.utils)
+(ns ingraph.sre.queries.utils
+  (:require [sre.plan.pattern :as pattern]
+            [taoensso.tufte :as tufte]))
 
 (defn get-tasks [pattern] (((nth pattern 3) (nth pattern 1))))
+
+(defn compile-pattern [def-sym k]
+  (let [def @(resolve def-sym)]
+    (tufte/profile {} (tufte/p (-> def-sym str keyword) (pattern/compile (:pattern def) (:config def) {:k k})))))
