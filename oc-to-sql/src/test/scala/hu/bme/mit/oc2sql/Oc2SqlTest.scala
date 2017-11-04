16e65b4eccb57383d6ae5c8ce0e5d158a0a9327b
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-04 16:27:38 -0400

    Add class use

--- oc-to-sql/src/test/scala/hu/bme/mit/oc2sql/Oc2SqlTest.scala
+++ oc-to-sql/src/test/scala/hu/bme/mit/oc2sql/Oc2SqlTest.scala
@@ -1,7 +1,12 @@
 package hu.bme.mit.oc2sql
 
+import org.cytosm.cypher2sql.PassAvailables
 import org.scalatest.FunSuite
 
 class Oc2SqlTest extends FunSuite {
+
+  test("use class") {
+    println(PassAvailables.parseCypher("MATCH (n) RETURN (n)"))
+  }
 
 }
