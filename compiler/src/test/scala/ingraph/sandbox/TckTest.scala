e82f65208eedb1feddb4d2299fcfe0bc0b16794c
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-20 10:47:31 -0500

    Add some relevant TCK test cases (no assertions yet)

--- compiler/src/test/scala/ingraph/sandbox/TckTest.scala
+++ compiler/src/test/scala/ingraph/sandbox/TckTest.scala
@@ -10,6 +10,8 @@ import org.scalatest.FunSuite
 
 class TckTest extends FunSuite {
 
+  val log = true
+
   case class CompilationStages(
     unresolvedQPlan: QNode,
     resolvedQPlan: QNode,
@@ -24,15 +26,23 @@ class TckTest extends FunSuite {
     val jPlan = QPlanToJPlan.transform(resolvedQPlan)
     val fPlan = SchemaInferencer.transform(jPlan)
 
-    println("=============================================================================")
-    println("Query:")
-    println(query)
-    println("-----------------------------------------------------------------------------")
-    println("Unresolved QPlan:")
-    println(unresolvedQPlan)
-    println("-----------------------------------------------------------------------------")
-    println("Resolved QPlan:")
-    println(resolvedQPlan)
+    if (log) {
+      println("=============================================================================")
+      println("Query:")
+      println(query)
+      println("-----------------------------------------------------------------------------")
+      println("Unresolved QPlan:")
+      println(unresolvedQPlan)
+      println("-----------------------------------------------------------------------------")
+      println("Resolved QPlan:")
+      println(resolvedQPlan)
+      println("-----------------------------------------------------------------------------")
+      println("JPlan:")
+      println(jPlan)
+      println("-----------------------------------------------------------------------------")
+      println("FPlan:")
+//      println(fPlan)
+    }
 
     return CompilationStages(unresolvedQPlan, resolvedQPlan, jPlan, fPlan)
   }
@@ -45,12 +55,87 @@ class TckTest extends FunSuite {
     )
   }
 
-  test("TCK test for MATCH 1") {
+  test("TCK test: Use multiple MATCH clauses to do a Cartesian product") {
     val stages = compile(
       """MATCH (n), (m)
         |RETURN n.value AS n, m.value AS m
       """.stripMargin
     )
   }
+
+  test("TCK test: Filter out based on node prop name") {
+    val stages = compile(
+      """MATCH ()-[rel:X]-(a)
+        |WHERE a.name = 'Andres'
+        |RETURN a
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Filter based on rel prop name") {
+    val stages = compile(
+      """MATCH (node)-[r:KNOWS]->(a)
+        |WHERE r.name = 'monkey'
+        |RETURN a
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Get neighbours") {
+    val stages = compile(
+      """MATCH (n1)-[rel:KNOWS]->(n2)
+        |RETURN n1, n2
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Get two related nodes") {
+    val stages = compile(
+      """MATCH ()-[rel:KNOWS]->(x)
+        |RETURN x
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Get related to related to") {
+    val stages = compile(
+      """MATCH (n)-->(a)-->(b)
+        |RETURN b
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Handle comparison between node properties") {
+    val stages = compile(
+      """MATCH (n)-[rel]->(x)
+        |WHERE n.animal = x.animal
+        |RETURN n, x
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Handle OR in the WHERE clause") {
+    val stages = compile(
+      """MATCH (n)
+        |WHERE n.p1 = 12 OR n.p2 = 13
+        |RETURN n
+      """.stripMargin
+    )
+  }
+
+  test("TCK test: Return relationships by collecting them as a list") {
+    val stages = compile(
+      """MATCH (a:Start)-[r:REL*2..2]-(b)
+        |RETURN r
+      """.stripMargin
+    )
+  }
+
+//  test("TCK test: ") {
+//    val stages = compile(
+//      """
+//      """.stripMargin
+//    )
+//  }
 
 }
