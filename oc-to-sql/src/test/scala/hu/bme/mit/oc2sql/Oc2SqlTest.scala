38409ba9d7fbabfc73804069e446c2bb23f0c6c8
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-21 16:35:45 -0500

    Add more test queries

--- oc-to-sql/src/test/scala/hu/bme/mit/oc2sql/Oc2SqlTest.scala
+++ oc-to-sql/src/test/scala/hu/bme/mit/oc2sql/Oc2SqlTest.scala
@@ -6,11 +6,42 @@ import org.scalatest.FunSuite
 
 class Oc2SqlTest extends FunSuite {
 
-  test("convert to SQL") {
+  test("PosLength") {
     val cypher = CypherParser.parseString(
       """MATCH (segment:Segment)
         |WHERE segment.length <= 0
         |RETURN segment, segment.length AS length
+        |""".stripMargin)
+    val qplan = CypherToQPlan.build(cypher)
+    println(qplan)
+  }
+
+  test("RouteSensor") {
+    val cypher = CypherParser.parseString(
+      """MATCH (route:Route)-[:follows]->(swP:SwitchPosition)-[:target]->(sw:Switch)-[:monitoredBy]->(sensor:Sensor)
+        |WHERE NOT (route)-[g:requires]->(sensor)
+        |RETURN route, sensor, swP, sw
+        |""".stripMargin)
+    val qplan = CypherToQPlan.build(cypher)
+    println(qplan)
+  }
+
+  test("SwitchSensor") {
+    val cypher = CypherParser.parseString(
+      """MATCH (sw:Switch)
+        |WHERE NOT (sw)-[:monitoredBy]->(:Sensor)
+        |RETURN sw
+        |""".stripMargin)
+    val qplan = CypherToQPlan.build(cypher)
+    println(qplan)
+  }
+
+  test("SwitchSet") {
+    val cypher = CypherParser.parseString(
+      """MATCH (semaphore:Semaphore)<-[:entry]-(route:Route)-[:follows]->(swP:SwitchPosition)-[:target]->(sw:Switch)
+        |WHERE semaphore.signal = "GO"
+        |  AND sw.currentPosition <> swP.position
+        |RETURN semaphore, route, swP, sw, sw.currentPosition AS currentPosition, swP.position AS position
         |""".stripMargin)
     val qplan = CypherToQPlan.build(cypher)
     println(qplan)
