7b2825187e4b0b4c5061fc1782e555952843a007
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-21 16:17:34 -0500

    Remove unused dep, add test for PosLength query

--- oc-to-sql/src/test/scala/hu/bme/mit/oc2sql/Oc2SqlTest.scala
+++ oc-to-sql/src/test/scala/hu/bme/mit/oc2sql/Oc2SqlTest.scala
@@ -1,12 +1,19 @@
 package hu.bme.mit.oc2sql
 
-import org.cytosm.cypher2sql.PassAvailables
+import ingraph.compiler.CypherToQPlan
+import ingraph.compiler.cypher2qplan.CypherParser
 import org.scalatest.FunSuite
 
 class Oc2SqlTest extends FunSuite {
 
-  test("use class") {
-    println(PassAvailables.parseCypher("MATCH (n) RETURN (n)"))
+  test("convert to SQL") {
+    val cypher = CypherParser.parseString(
+      """MATCH (segment:Segment)
+        |WHERE segment.length <= 0
+        |RETURN segment, segment.length AS length
+        |""".stripMargin)
+    val qplan = CypherToQPlan.build(cypher)
+    println(qplan)
   }
 
 }
