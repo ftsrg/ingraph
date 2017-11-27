ebd5e0392cb47a868ff1f6fa374f7e1adbb2958d
Bálint Hegyi <hegyi.balint.b@gmail.com>
2017-11-27 14:53:27 +0100

    Pretty print LdbcSnbTest

--- tests/src/main/scala/ingraph/tests/LdbcSnbTest.scala
+++ tests/src/main/scala/ingraph/tests/LdbcSnbTest.scala
@@ -54,46 +54,46 @@ abstract class LdbcSnbTest extends FunSuite {
   val testCases: Map[TestCase, Map[String, String]] =
     Map(
       TestCase("bi", 1) -> Map("date" -> 20110721220000000),
-      TestCase("bi", 2) -> Map("date1" -> 20081231230000000,"date2" -> 20161107230000000,"country1" -> India,"country2" -> China),
-      TestCase("bi", 3) -> Map("year" -> 2011,"month" -> 6),
-      TestCase("bi", 4) -> Map("tagClass" -> Person,"country" -> Austria),
+      TestCase("bi", 2) -> Map("date1" -> 20081231230000000, "date2" -> 20161107230000000, "country1" -> India, "country2" -> China),
+      TestCase("bi", 3) -> Map("year" -> 2011, "month" -> 6),
+      TestCase("bi", 4) -> Map("tagClass" -> Person, "country" -> Austria),
       TestCase("bi", 5) -> Map("country" -> India),
       TestCase("bi", 6) -> Map("tag" -> Hamid_Karzai),
       TestCase("bi", 7) -> Map("tag" -> Wolfgang_Amadeus_Mozart),
       TestCase("bi", 8) -> Map("tag" -> Wolfgang_Amadeus_Mozart),
-      TestCase("bi", 9) -> Map("tagClass1" -> OfficeHolder,"tagClass2" -> Person,"threshold" -> 1),
-      TestCase("bi", 10) -> Map("tag" -> Hamid_Karzai,"date" -> 20110817060540590),
-      TestCase("bi", 11) -> Map("country" -> India,"blacklist" -> one,has,David),
-      TestCase("bi", 12) -> Map("date" -> 20110817060540590,"likeThreshold" -> 1),
+      TestCase("bi", 9) -> Map("tagClass1" -> OfficeHolder, "tagClass2" -> Person, "threshold" -> 1),
+      TestCase("bi", 10) -> Map("tag" -> Hamid_Karzai, "date" -> 20110817060540590),
+      TestCase("bi", 11) -> Map("country" -> India, "blacklist" -> one, has, David),
+      TestCase("bi", 12) -> Map("date" -> 20110817060540590, "likeThreshold" -> 1),
       TestCase("bi", 13) -> Map("country" -> India),
-      TestCase("bi", 14) -> Map("begin" -> 2008817060540595,"end" -> 20160817060540596),
+      TestCase("bi", 14) -> Map("begin" -> 2008817060540595, "end" -> 20160817060540596),
       TestCase("bi", 15) -> Map("country" -> India),
-      TestCase("bi", 16) -> Map("personId" -> 0,"country" -> India,"tagClass" -> OfficeHolder,"minPathDistance" -> 1,"maxPathDistance" -> 2),
+      TestCase("bi", 16) -> Map("personId" -> 0, "country" -> India, "tagClass" -> OfficeHolder, "minPathDistance" -> 1, "maxPathDistance" -> 2),
       TestCase("bi", 17) -> Map("country" -> Austria),
-      TestCase("bi", 18) -> Map("date" -> 20080822040000000,"lengthThreshold" -> 240,"languages" -> uz),
-      TestCase("bi", 19) -> Map("date" -> 1,"tagClass1" -> Person,"tagClass2" -> OfficeHolder),
+      TestCase("bi", 18) -> Map("date" -> 20080822040000000, "lengthThreshold" -> 240, "languages" -> uz),
+      TestCase("bi", 19) -> Map("date" -> 1, "tagClass1" -> Person, "tagClass2" -> OfficeHolder),
       TestCase("bi", 20) -> Map("tagClasses" -> Person),
-      TestCase("bi", 21) -> Map("country" -> India,"endDate" -> 20100128063958900),
-      TestCase("bi", 22) -> Map("country1" -> India,"country2" -> USA),
+      TestCase("bi", 21) -> Map("country" -> India, "endDate" -> 20100128063958900),
+      TestCase("bi", 22) -> Map("country1" -> India, "country2" -> USA),
       TestCase("bi", 23) -> Map("country" -> India),
       TestCase("bi", 24) -> Map("tagClass" -> OfficeHolder),
-      TestCase("bi", 25) -> Map("person1Id" -> 0,"person2Id" -> 2,"startDate" -> 20000601040000000,"endDate" -> 20190701040000000),
+      TestCase("bi", 25) -> Map("person1Id" -> 0, "person2Id" -> 2, "startDate" -> 20000601040000000, "endDate" -> 20190701040000000),
 
     )
 
   testCases.foreach(
     t =>
       test(s"${t._1.workload}-${t._1.number}-size-1") {
-      val parameters = t._2
+        val parameters = t._2
 
-      val queryName = s"ldbc-snb-${t._1.workload}-${t._1.number}"
+        val queryName = s"ldbc-snb-${t._1.workload}-${t._1.number}"
 
-      val baseQuerySpecification = Source.fromFile(queryPath(t._1.workload, t._1.number)).getLines().mkString("\n")
-      val querySpecification = parameters.foldLeft(baseQuerySpecification)((a, b) => a.replaceAllLiterally(b._1, b._2))
-      println(querySpecification)
+        val baseQuerySpecification = Source.fromFile(queryPath(t._1.workload, t._1.number)).getLines().mkString("\n")
+        val querySpecification = parameters.foldLeft(baseQuerySpecification)((a, b) => a.replaceAllLiterally(b._1, b._2))
+        println(querySpecification)
 
-      //runQuery(t._1.workload, t._1.number, queryName, querySpecification)
-    })
+        //runQuery(t._1.workload, t._1.number, queryName, querySpecification)
+      })
 
   def runQuery(workload: String, queryNumber: Int, queryName: String, querySpecification: String)
 }
