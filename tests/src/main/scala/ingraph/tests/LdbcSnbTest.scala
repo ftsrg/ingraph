507c1c72a85ad837be32b8125d813da793019711
Bálint Hegyi <hegyi.balint.b@gmail.com>
2017-11-28 20:27:10 +0100

    Proceeding with LdbcSnbTest, most of them in working order

--- tests/src/main/scala/ingraph/tests/LdbcSnbTest.scala
+++ tests/src/main/scala/ingraph/tests/LdbcSnbTest.scala
@@ -1,68 +1,63 @@
 package ingraph.tests
 
+import apoc.export.graphml.ExportGraphML
+import apoc.graph.Graphs
 import com.google.gson.Gson
-import jdk.nashorn.internal.parser.JSONParser
-import org.scalatest.{FunSuite, Ignore}
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.factory.GraphDatabaseSettings
+import org.neo4j.kernel.api.exceptions.KernelException
+import org.neo4j.kernel.impl.proc.Procedures
+import org.neo4j.kernel.internal.GraphDatabaseAPI
+import org.neo4j.test.TestGraphDatabaseFactory
+import org.scalatest.FunSuite
 
+import scala.collection.JavaConversions
 import scala.io.Source
-import scala.util.parsing.json.{JSON, JSONObject, JSONType}
 
-abstract class LdbcSnbTest extends FunSuite {
+class LdbcSnbTest extends FunSuite {
+
+  val testCases: Seq[TestCase] = 1 to 25 map (TestCase("bi", _))
+
+  testCases.foreach(
+    t =>
+      test(s"${t.workload}-${t.number}-size-1") {
+        val parameters = t.parameters
+
+        val queryName = s"ldbc-snb-${t.workload}-${t.number}"
+        println(queryName)
+
+        val baseQuerySpecification = readToString(queryPath(t.workload, t.n))
+        println(parameters)
+        val querySpecification = parameters.foldLeft(baseQuerySpecification)((a, b) =>
+          a.replaceAllLiterally("$" + b._1.toString, b._2.toString))
+
+        runQuery(t.workload, t.number, queryName, querySpecification, parameters.toMap)
+      }
+  )
 
   def modelPath(entityName: String)(implicit number: Int) = s"../graphs/bi/$number/${entityName}_0_0.csv"
+
   def parameterPath(number: String) = s"../graphs/bi/$number/parameters"
 
   def queryPath(workload: String, query: Int): String = s"../queries/ldbc-snb-${workload}/${workload}-${query}.cypher"
 
-
   def queryResultPath(workload: String, query: Int): String = queryPath(workload, query).dropRight(".cypher".length) + "-50.bin"
 
-  val nodeFilenames: Int => Map[String, List[String]] = (n: Int) => {
-    implicit val x = n
-    Map(
-      modelPath("comment") -> List("Message", "Comment"),
-      modelPath("forum") -> List("Forum"),
-      modelPath("organisation") -> List("Organisation"),
-      modelPath("person") -> List("Person"),
-      modelPath("place") -> List("Place"),
-      modelPath("post") -> List("Message", "Post"),
-      modelPath("tagclass") -> List("TagClass"),
-      modelPath("tag") -> List("Tag")
-    )
+  @throws[KernelException]
+  def registerProcedure(db: GraphDatabaseService, procedures: Class[_]*): Unit = {
+    val proceduresService = db.asInstanceOf[GraphDatabaseAPI].getDependencyResolver.resolveDependency(classOf[Procedures])
+    for (procedure <- procedures) {
+      proceduresService.registerProcedure(procedure)
+      proceduresService.registerFunction(procedure)
+    }
   }
 
-  val relationshipFilenames = (n: Int) => {
-    implicit val x = n
-    Map(
-      modelPath("comment_hasCreator_person") -> "HAS_CREATOR",
-      modelPath("comment_isLocatedIn_place") -> "IS_LOCATED_IN",
-      modelPath("comment_replyOf_comment") -> "REPLY_OF",
-      modelPath("comment_replyOf_post") -> "REPLY_OF",
-      modelPath("forum_containerOf_post") -> "CONTAINER_OF",
-      modelPath("forum_hasMember_person") -> "HAS_MEMBER",
-      modelPath("forum_hasModerator_person") -> "HAS_MODERATOR",
-      modelPath("forum_hasTag_tag") -> "HAS_TAG",
-      modelPath("person_hasInterest_tag") -> "HAS_INTEREST",
-      modelPath("person_isLocatedIn_place") -> "IS_LOCATED_IN",
-      modelPath("person_knows_person") -> "KNOWS",
-      modelPath("person_likes_comment") -> "LIKES",
-      modelPath("person_likes_post") -> "LIKES",
-      modelPath("place_isPartOf_place") -> "IS_PART_OF",
-      modelPath("post_hasCreator_person") -> "HAS_CREATOR",
-      modelPath("comment_hasTag_tag") -> "HAS_TAG",
-      modelPath("post_hasTag_tag") -> "HAS_TAG",
-      modelPath("post_isLocatedIn_place") -> "IS_LOCATED_IN",
-      modelPath("tagclass_isSubclassOf_tagclass") -> "IS_SUBCLASS_OF",
-      modelPath("tag_hasType_tagclass") -> "HAS_TYPE",
-      modelPath("organisation_isLocatedIn_place") -> "IS_LOCATED_IN",
-      modelPath("person_studyAt_organisation") -> "STUDY_OF",
-      modelPath("person_workAt_organisation") -> "WORK_AT"
-    )
-  }
-  val gson = new Gson()
   case class TestCase(workload: String, n: Int) {
     val number = f"$n%02d"
+
     import scala.collection.JavaConverters._
+
+    val gson = new Gson()
     val parameters = gson
       .fromJson(readToString(parameterPath(number)), classOf[java.util.Map[String, Object]])
       .asScala
@@ -79,25 +74,29 @@ abstract class LdbcSnbTest extends FunSuite {
 
   def readToString(path: String): String = Source.fromFile(s"$path").getLines().mkString("\n")
 
-  val testCases: Seq[TestCase] = 1 to 25 map(TestCase("bi", _))
+  def runQuery(workload: String, queryNumber: String, queryName: String, querySpecification: String, parameters: Map[String, Object]): Unit = {
+    val bolt = GraphDatabaseSettings
+      .boltConnector("0")
 
-  testCases.foreach(
-    t =>
-      test(s"${t.workload}-${t.number}-size-1") {
-        val parameters = t.parameters
+    val gds = new TestGraphDatabaseFactory()
+      .newImpermanentDatabaseBuilder
+      .setConfig("apoc.import.file.enabled", "true")
+      .setConfig(bolt.`type`, "BOLT")
+      .setConfig(bolt.enabled, "true")
+      .setConfig(bolt.address, "localhost:7688")
+      .newGraphDatabase
 
-        val queryName = s"ldbc-snb-${t.workload}-${t.number}"
+    registerProcedure(gds, classOf[ExportGraphML], classOf[Graphs])
 
-        val baseQuerySpecification = readToString(queryPath(t.workload, t.n))
-        println(parameters)
-        val querySpecification = parameters.foldLeft(baseQuerySpecification)((a, b) =>
-          a.replaceAllLiterally("$" + b._1.toString, b._2.toString))
-        println(querySpecification)
-
-        runQuery(t.workload, t.number, queryName, querySpecification)
-      })
-
-  def runQuery(workload: String, queryNumber: String, queryName: String, querySpecification: String): Unit = {
-    throw new Exception(s"$workload/$queryNumber not implemented")
+    val trans = gds.beginTx()
+    try {
+      gds.execute(s"CALL apoc.import.graphml('../graphs/bi/graphmls/$workload-$queryNumber.graphml', {batchSize: 10000, readLabels: true})")
+      val results = gds.execute(querySpecification)
+      while(results.hasNext) println(results.next())
+      gds.execute(querySpecification, JavaConversions.mapAsJavaMap(parameters))
+    } finally {
+      trans.close()
+      gds.shutdown()
+    }
   }
 }
