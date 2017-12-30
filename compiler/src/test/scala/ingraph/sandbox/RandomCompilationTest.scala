47dc2b459053a5658371a3f017e2d4ff750e578d
Jozsef Marton <marton@db.bme.hu>
2017-12-30 23:52:11 +0100

    Added some random tests from git stash

--- compiler/src/test/scala/ingraph/sandbox/RandomCompilationTest.scala
+++ compiler/src/test/scala/ingraph/sandbox/RandomCompilationTest.scala
@@ -37,4 +37,56 @@ class RandomCompilationTest extends CompilerTest {
         |RETURN a, e, b""".stripMargin)
 
   }
+
+  test("Random: no edge variable in pattern") {
+    compile(
+      """MATCH (a)
+        |OPTIONAL MATCH (a)-->(b)
+        |WHERE 1=2
+        |RETURN b""".stripMargin
+    )
+  }
+
+  ignore("Random: create w/ vertex and edge properties") {
+    compile(
+      """CREATE (:Foo {a: 23, b: 'bar'})-[:FooEdge {a:11, c:'edge'}]->(:Foo2), (:Foo3)
+        |RETURN 1 AS one""".stripMargin
+    )
+  }
+
+  test("Random: vertex pattern with property map") {
+    compile(
+      """MATCH (n {id: "n1"})
+        |WHERE n.length=1
+        |RETURN n""".stripMargin
+    )
+  }
+
+  ignore("Random: create after match") {
+    compile(
+      """MATCH (n:Foo)
+        |CREATE (n)-[:Bar]->(m:Foo2)-[:Bar2]->(o:Foo3)
+        |RETURN n, m""".stripMargin
+    )
+  }
+
+  ignore("Random: various expressions") {
+    compile(
+      """MATCH (segment:Segment), (foo:Foo), (bar:Bar)
+        |WHERE 1=1
+        |  AND segment.name STARTS WITH 'alibaba'
+        |  AND 'rablo' ENDS WITH segment.name
+        |  AND 'mese' CONTAINS 'uveghegy'
+        |  AND segment.length <= 0
+        |  AND 2<3
+        |  AND NOT 1=1
+        |  OR 0=1
+        |  OR 1=1
+        |  AND sin(3)+4*2^6 > segment.length
+        |  AND sin(segment.length) > 2
+        |  AND segment.length IS NULL
+        |RETURN DISTINCT *, sum(segment.width) as w, segment as l, [1, segment.length, cos(3)] as list, count(*) as ize
+        |ORDER BY 2+3 DESC, l.width ASC""".stripMargin
+    )
+  }
 }
