87dd5586031bf0692e98995bd329d29e47aa65ce
Jozsef Marton <marton@db.bme.hu>
2018-02-11 18:29:35 +0100

    Refactor DmlTest to make use of base class CompilerTest

--- compiler/src/test/scala/ingraph/sandbox/DmlTest.scala
+++ compiler/src/test/scala/ingraph/sandbox/DmlTest.scala
@@ -7,45 +7,31 @@ import ingraph.model.fplan
 class DmlTest extends CompilerTest {
 
   test("should compile simple DELETE for a vertex") {
-    val cypher = CypherParser.parseString("MATCH (n:Person) DELETE n")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (n:Person) DELETE n")
   }
 
   test("should compile simple DELETE for an edge") {
-    val cypher = CypherParser.parseString("MATCH (p1:Person)-[r:KNOWS]-(p2:Person) DELETE r")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (p1:Person)-[r:KNOWS]-(p2:Person) DELETE r")
   }
 
   test("should compile simple DELETE for an entire pattern") {
-    val cypher = CypherParser.parseString("MATCH (p1:Person)-[r:KNOWS]-(p2:Person) DELETE p1, r, p2")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (p1:Person)-[r:KNOWS]-(p2:Person) DELETE p1, r, p2")
   }
 
   test("should compile vanilla CREATE") {
-    val cypher = CypherParser.parseString("CREATE (p:Person)")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (p:Person)")
   }
 
   test("should compile vanilla CREATE with attributes") {
-    val cypher = CypherParser.parseString("CREATE (p:Person {name: 'Batman'})")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (p:Person {name: 'Batman'})")
   }
 
   test("should compile vanilla CREATE with attributes 2") {
-    val cypher = CypherParser.parseString("CREATE (t:Train {a: 1})")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (t:Train {a: 1})")
   }
 
   test("should compile CREATE with a single edges") {
-    val cypher = CypherParser.parseString("CREATE (n:Person)-[:LIVES]->(c:City)")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (n:Person)-[:LIVES]->(c:City)")
   }
 
   test("should compile CREATE with two edges") {
@@ -89,45 +75,31 @@ class DmlTest extends CompilerTest {
   }
 
   test("should compile CREATE with property map, string literals only") {
-    val cypher = CypherParser.parseString("CREATE (n)-[:KNOWS {since: 'Yesterday'} ]->(:Person {name: 'A'} )")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (n)-[:KNOWS {since: 'Yesterday'} ]->(:Person {name: 'A'} )")
   }
 
   test("should compile CREATE with property map, number literals only") {
-    val cypher = CypherParser.parseString("CREATE (n)-[:KNOWS {strength: 7} ]->(:Person {age: 25, weight: 70} )")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (n)-[:KNOWS {strength: 7} ]->(:Person {age: 25, weight: 70} )")
   }
 
   test("should compile CREATE with property map with a mix of literals and expressions") {
-    val cypher = CypherParser.parseString("CREATE (n)-[:KNOWS {since: 'Yesterday'} ]->(:Person {name: 'A', age: 3+5} )")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("CREATE (n)-[:KNOWS {since: 'Yesterday'} ]->(:Person {name: 'A', age: 3+5} )")
   }
 
   test("should compile CREATE after MATCH") {
-    val cypher = CypherParser.parseString("MATCH (n) CREATE (n)-[:KNOWS]->(:Person)")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (n) CREATE (n)-[:KNOWS]->(:Person)")
   }
 
   test("should compile CREATE after MATCH on two nodes") {
-    val cypher = CypherParser.parseString("MATCH (n:Person), (c:City) CREATE (n)-[:KNOWS]->(c)")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (n:Person), (c:City) CREATE (n)-[:KNOWS]->(c)")
   }
 
   ignore("should compile REMOVE that targets labels") {
-    val cypher = CypherParser.parseString("MATCH (n) REMOVE n:Actor:Director")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (n) REMOVE n:Actor:Director")
   }
 
   ignore("should compile SET that targets labels") {
-    val cypher = CypherParser.parseString("MATCH (n) SET n:Actor:Director")
-    val plan = CypherToQPlan.build(cypher)
-    println(plan)
+    compile("MATCH (n) SET n:Actor:Director")
   }
 
 }
