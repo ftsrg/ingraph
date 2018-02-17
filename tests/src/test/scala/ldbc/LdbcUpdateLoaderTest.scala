beecc1125bf569cf70c02267da83c8104d9ac603
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-17 22:32:47 +0100

    Use implicit conversions. Don't try this at home

--- tests/src/test/scala/ldbc/LdbcUpdateLoaderTest.scala
+++ tests/src/test/scala/ldbc/LdbcUpdateLoaderTest.scala
@@ -7,9 +7,11 @@ import scala.collection.JavaConverters._
 
 class LdbcUpdateLoaderTest extends FunSuite {
 
-//  implicit def longs(x: Any): Long = x.asInstanceOf[Long]
-//  implicit def string(x: Any): String = x.asInstanceOf[String]
-//  implicit def int(x: Any): Int = x.asInstanceOf[Int]
+  implicit def longs(x: Any): Long = x.asInstanceOf[Long]
+  implicit def string(x: Any): String = x.asInstanceOf[String]
+  implicit def int(x: Any): Int = x.asInstanceOf[Int]
+  implicit def longList(x: Any): List[Long] = x.asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[Long]).toList
+  implicit def stringList(x: Any): List[String] = x.asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[String]).toList
 
   val CSV_DIR: String = "../graphs/ldbc-snb-bi/update-streams/"
 
@@ -20,79 +22,15 @@ class LdbcUpdateLoaderTest extends FunSuite {
       val u = update.asScala
       val updateType = u(2).asInstanceOf[Int]
 
-      import scala.language.implicitConversions
-
       updateType match {
-        case 1 => Update1AddPerson(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[String],
-          u(5).asInstanceOf[String],
-          u(6).asInstanceOf[String],
-          u(7).asInstanceOf[Long],
-          u(8).asInstanceOf[Long],
-          u(9).asInstanceOf[String],
-          u(10).asInstanceOf[String],
-          u(11).asInstanceOf[Long],
-          u(12).asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[String]).toList,
-          u(13).asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[String]).toList,
-          u(14).asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[Long]).toList,
-          null,
-          null
-        )
-        case 2 => Update2AddPostLike(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[Long],
-          u(5).asInstanceOf[Long]
-        )
-        case 3 => Update3AddCommentLike(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[Long],
-          u(5).asInstanceOf[Long]
-        )
-        case 4 => Update4AddForum(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[String],
-          u(5).asInstanceOf[Long],
-          u(6).asInstanceOf[Long],
-          u(7).asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[Long]).toList
-        )
-        case 5 => Update5AddForumMembership(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[Long],
-          u(5).asInstanceOf[Long]
-        )
-        case 6 => Update6AddPost(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[String],
-          u(5).asInstanceOf[Long],
-          u(6).asInstanceOf[String],
-          u(7).asInstanceOf[String],
-          u(8).asInstanceOf[String],
-          u(9).asInstanceOf[String],
-          u(10).asInstanceOf[Int],
-          u(11).asInstanceOf[Long],
-          u(12).asInstanceOf[Long],
-          u(13).asInstanceOf[Long],
-          u(14).asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[Long]).toList
-        )
-        case 7 => Update7AddComment(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[Long],
-          u(5).asInstanceOf[String],
-          u(6).asInstanceOf[String],
-          u(7).asInstanceOf[String],
-          u(8).asInstanceOf[Int],
-          u(9).asInstanceOf[Long],
-          u(10).asInstanceOf[Long],
-          u(11).asInstanceOf[Long],
-          u(12).asInstanceOf[Long],
-          u(13).asInstanceOf[java.util.List[Object]].asScala.map(_.asInstanceOf[Long]).toList
-        )
-        case 8 => Update8AddFriendship(
-          u(3).asInstanceOf[Long],
-          u(4).asInstanceOf[Long],
-          u(5).asInstanceOf[Long]
-        )
+        case 1 => Update1AddPerson         (u(3), u(4), u(5), u(6), u(7), u(8), u(9), u(10), u(11), u(12), u(13), u(14), null, null)
+        case 2 => Update2AddPostLike       (u(3), u(4), u(5))
+        case 3 => Update3AddCommentLike    (u(3), u(4), u(5))
+        case 4 => Update4AddForum          (u(3), u(4), u(5), u(6), u(7))
+        case 5 => Update5AddForumMembership(u(3), u(4), u(5))
+        case 6 => Update6AddPost           (u(3), u(4), u(5), u(6), u(7), u(8), u(9), u(10), u(11), u(12), u(13), u(14))
+        case 7 => Update7AddComment        (u(3), u(4), u(5), u(6), u(7), u(8), u(9), u(10), u(11), u(12), u(13))
+        case 8 => Update8AddFriendship     (u(3), u(4), u(5))
       }
     }
 
