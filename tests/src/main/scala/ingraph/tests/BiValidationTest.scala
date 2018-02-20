3eb7cf58ce0098bc72d52a92ec7d6a905db38e30
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-20 12:52:01 +0100

    Rename test class, only test for queries that can instantiate the engine

--- tests/src/main/scala/ingraph/tests/BiValidationTest.scala
+++ tests/src/main/scala/ingraph/tests/BiValidationTest.scala
@@ -2,9 +2,9 @@ package ingraph.tests
 
 import org.scalatest.FunSuite
 
-class LdbcSnbBiTest extends FunSuite {
+class BiValidationTest extends FunSuite {
 
-  val testCases: Seq[LdbcSnbTestCase] = 1 to 25 map (new LdbcSnbTestCase("bi", _))
+  val testCases: Seq[LdbcSnbTestCase] = Seq(2, 3, 4, 6, 7, 8, 9, 12, 15, 17, 23, 24) map (new LdbcSnbTestCase("bi", _))
 
   testCases.foreach {
     tc =>
