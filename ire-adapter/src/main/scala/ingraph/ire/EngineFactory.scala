63ff8fb255a2ca2947e5c62dbb64040a516ebfff
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-20 19:39:26 +0100

    Debugging BI Q2

--- ire-adapter/src/main/scala/ingraph/ire/EngineFactory.scala
+++ ire-adapter/src/main/scala/ingraph/ire/EngineFactory.scala
@@ -139,6 +139,9 @@ object EngineFactory {
     // unary nodes
 
     private def grouping(op: Grouping, expr: ForwardConnection) = {
+      println(op.projectionTuple)
+      println(op.aggregationCriteria)
+
       val aggregates = op.projectionTuple.map(e => ExpressionParser.parseAggregate(e))
       val factories = aggregates.flatten.map(_._2()).toVector
       val nonAggregates = op.projectionTuple.filter(op.aggregationCriteria.contains)
--- tests/src/main/scala/ingraph/tests/BiValidationTest.scala
+++ tests/src/main/scala/ingraph/tests/BiValidationTest.scala
@@ -4,23 +4,22 @@ import org.scalatest.FunSuite
 
 class BiValidationTest extends FunSuite {
 
-  val testCases: Seq[LdbcSnbTestCase] = Seq(2, 3, 4, 6, 7, 8, 9, 12, 15, 17, 23, 24) map (new LdbcSnbTestCase("bi", _))
+  val testCases: Seq[LdbcSnbTestCase] = Seq(2
+  //  , 3, 4, 6, 7, 8, 9, 12, 15, 17, 23, 24
+  ) map (new LdbcSnbTestCase("bi", _))
 
   testCases.foreach {
     tc =>
       test(s"${tc.name}") {
-        val neo4jResults = TestRunners.neo4jTestRunner(tc)
+        val neo4jResults   = TestRunners.neo4jTestRunner(tc)
         val ingraphResults = TestRunners.ingraphTestRunner(tc)
 
         println("neo4j results:   " + neo4jResults)
         println("ingraph results: " + ingraphResults)
 
-        //val ingraphResult = ingraphRunner.getResults()
-
-        //println(tc.parameters.mkString("Parameters: (\n\t", "\n\t", "\n)"))
-        //println(neo4jResult.mkString("Results: (\n\t", "\n\t", "\n)"))
-
-        //assert(neo4jResult.equals(ingraphResults))
+//        println(tc.parameters.mkString("Parameters: (\n\t", "\n\t", "\n)"))
+//        println(neo4jResult.mkString("Results: (\n\t", "\n\t", "\n)"))
+//        assert(neo4jResults.equals(ingraphResults))
       }
   }
 
