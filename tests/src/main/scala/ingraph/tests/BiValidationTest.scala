2788aa0b03de39ff684fc976c1d004a1b5251a79
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-23 21:13:52 +0100

    Update test class to reflect the current state of engine

--- tests/src/main/scala/ingraph/tests/BiValidationTest.scala
+++ tests/src/main/scala/ingraph/tests/BiValidationTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class BiValidationTest extends FunSuite {
 
-  val collects = Seq(5, 10, 21)
+  val collects = Seq(10)
   val collectLists = Seq(13, 22)
   val transitives = Seq(14, 16, 18, 19, 20)
   val metaFeatures = Seq(1)
@@ -12,7 +12,9 @@ class BiValidationTest extends FunSuite {
   val listComprehensions = Seq(11)
   val madness = Seq(25)
 
-  val testCases: Seq[LdbcSnbTestCase] = Seq(2, 4, 6, 7, 8, 9, 12, 17, 23, 24) map (new LdbcSnbTestCase("bi", _))
+  val working = Seq(2, 4, 5, 6, 7, 8, 9, 12, 17, 21, 23, 24)
+
+  val testCases: Seq[LdbcSnbTestCase] = collects map (new LdbcSnbTestCase("bi", _))
 
   testCases.foreach {
     tc =>
@@ -23,8 +25,6 @@ class BiValidationTest extends FunSuite {
         println("neo4j results:   " + neo4jResults  .map(x => x.toSeq.sortBy(_._1)))
         println("ingraph results: " + ingraphResults.map(x => x.toSeq.sortBy(_._1)))
 
-//        println(tc.parameters.mkString("Parameters: (\n\t", "\n\t", "\n)"))
-//        println(neo4jResult.mkString("Results: (\n\t", "\n\t", "\n)"))
         assert(ingraphResults == neo4jResults)
       }
   }
