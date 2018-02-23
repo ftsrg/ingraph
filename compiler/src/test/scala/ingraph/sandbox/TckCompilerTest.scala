f1fa38b7b07efbec34d1ea166aefa4e62ce73fef
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-24 00:45:28 +0100

    Revert "Test if edge lists attributes are resolved"
    This reverts commit bab09f1637e82a6dd54e2ee27990a47935ccc8ef.

--- compiler/src/test/scala/ingraph/sandbox/TckCompilerTest.scala
+++ compiler/src/test/scala/ingraph/sandbox/TckCompilerTest.scala
@@ -1,7 +1,5 @@
 package ingraph.sandbox
 
-import ingraph.model.qplan.Expand
-
 class TckCompilerTest extends CompilerTest {
 
   test("Hello World") {
@@ -134,13 +132,11 @@ class TckCompilerTest extends CompilerTest {
     assert(getLeafNodes(stages.fplan)(0).extraAttributes.length == 0)
   }
 
-  test("Transitive closure resolved") {
-    val stages = compile(
-      """MATCH (post:Post)-[:REPLY_OF*]->(message:Message)
-        |RETURN post.id, message.id
-      """.stripMargin
-    )
-    assert(stages.qplan.children(0).children(0).children(0).asInstanceOf[Expand].edge.resolved == true)
-  }
+//  test("TCK test: ") {
+//    val stages = compile(
+//      """
+//      """.stripMargin
+//    )
+//  }
 
 }
