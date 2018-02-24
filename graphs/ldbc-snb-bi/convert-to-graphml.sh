36aa523a893a16a8a3489c21495889b7d2a5cd6e
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-24 01:40:00 +0100

    Revert convert script, fix typo

--- graphs/ldbc-snb-bi/convert-to-graphml.sh
+++ graphs/ldbc-snb-bi/convert-to-graphml.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#rm -rf graphmls
-#mkdir graphmls
+rm -rf graphmls
+mkdir graphmls
 
-#for i in 0{1..9} {10..25} ; do
-for i in 14; do
+for i in 0{1..9} {10..25} ; do
   echo ================================================================================
   echo Procesing data set for query $i.
   echo ================================================================================
@@ -16,7 +15,7 @@ for i in 14; do
   export DATA_DIR=`pwd`/$i
   ./import-to-neo4j.sh
   $NEO4J_HOME/bin/neo4j restart
-  # 5 seconds of sleep was not enough and i7/SSD laptop
+  # 5 seconds of sleep was not enough on an i7/SSD laptop
   sleep 10
 
   $NEO4J_HOME/bin/cypher-shell -u neo4j -p admin "CALL apoc.export.graphml.all('graphmls/bi-$i.graphml', {useTypes: true})"
