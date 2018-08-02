7c25b83b1110248ef37ce0d93343116394175792
Gabor Szarnyas <szarnyasg@gmail.com>
2018-08-02 21:46:34 +0200

    Remove unused parallel joins class

--- ire/src/main/scala/hu/bme/mit/ire/nodes/binary/JoinNode.scala
+++ ire/src/main/scala/hu/bme/mit/ire/nodes/binary/JoinNode.scala
@@ -11,18 +11,3 @@ class JoinNode(override val next: (ReteMessage) => Unit,
                override val secondaryMask: Mask
               ) extends JoinNodeBase with SingleForwarder {
 }
-
-class ParallelJoinNode(override val children: Vector[(ReteMessage) => Unit],
-                       override val primaryTupleWidth: Int,
-                       override val secondaryTupleWidth: Int,
-                       override val primaryMask: Mask,
-                       override val secondaryMask: Mask,
-                       hashFunction: (Tuple) => Int = n => n.hashCode()
-                      ) extends JoinNodeBase with ForkingForwarder {
-
-  override def forwardHashFunction(n: Tuple): Int = hashFunction(n)
-
-  override def forward(cs: ChangeSet): Unit = super[ForkingForwarder].forward(cs)
-
-  override def forward(t: TerminatorMessage): Unit = super[ForkingForwarder].forward(t)
-}
