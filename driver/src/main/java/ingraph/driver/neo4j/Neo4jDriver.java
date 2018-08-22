df81dacc231da5fa12006cb9c8afb910b2f15f11
marci543 <marci543@users.noreply.github.com>
2018-08-22 12:16:25 +0200

    Change DB folder on retry in Neo4jDriver for tests

--- driver/src/main/java/ingraph/driver/neo4j/Neo4jDriver.java
+++ driver/src/main/java/ingraph/driver/neo4j/Neo4jDriver.java
@@ -10,6 +10,8 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
+import java.io.File;
+
 public class Neo4jDriver extends CypherDriver {
 
 	final Driver driver;
@@ -19,16 +21,17 @@ public class Neo4jDriver extends CypherDriver {
 		GraphDatabaseSettings.BoltConnector bolt = GraphDatabaseSettings.boltConnector("0");
 
 		String host = "localhost";
-		int port = 7687; // this is the default Neo4j port - we start one higher than this to avoid collisions
+		final int basePort = 7687; // this is the default Neo4j port - we start one higher than this to avoid collisions
+		final int maximumRetry = 100;
 		GraphDatabaseService graphDbTemp = null;
 		String address = null;
-		while (port < 65000) {
-			port++;
+		for (int retryCount = 1; true; retryCount++) {
+			final int port = basePort + retryCount;
 			address = String.format("%s:%d", host, port);
 			System.out.println("instantiating database listening on " + address);
 			try {
 				graphDbTemp = new TestGraphDatabaseFactory()
-					.newImpermanentDatabaseBuilder()
+					.newImpermanentDatabaseBuilder(new File("target/test-data/impermanent-db-" + retryCount))
 					.setConfig(bolt.type, "BOLT")
 					.setConfig(bolt.enabled, "true")
 					.setConfig(bolt.address, address)
@@ -36,6 +39,9 @@ public class Neo4jDriver extends CypherDriver {
 			} catch (RuntimeException e) {
 				// this is usually a org.neo4j.kernel.lifecycle.LifecycleException
 				// caused by org.neo4j.helpers.PortBindException
+				if (retryCount >= maximumRetry)
+					throw e;
+
 				e.printStackTrace();
 				System.out.println("Cannot connect on port " + port + ", retrying on a higher port.");
 				if (graphDbTemp != null)
