37464671c7bb55bb3df7add12dc450e4ddebc6fd
Gabor Szarnyas <szarnyasg@gmail.com>
2018-09-23 19:52:36 +0200

    Tee/pipe output of benchmark script to file

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -7,9 +7,12 @@ QUERIES="2 3 4 6 8 9 12 15 22 23 24"
 mv results.csv results-`date +%s`.csv
 
 for SF in $SFS; do
+	echo scale factor: $SF
 	for RUN in $RUNS; do
+		echo run: $RUN
 		for QUERY in $QUERIES; do
-			./tests $SF $QUERY
+			echo query: $QUERY
+			./tests $SF $QUERY | tee results.csv
 		done
 	done
 done
