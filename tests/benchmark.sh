7c5a978b7bf6f1f561e1e34cba193a187948131e
Gabor Szarnyas <szarnyasg@gmail.com>
2018-09-24 07:55:13 +0200

    Echo JAVA_OPTS before running the benchmark

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -6,6 +6,7 @@ QUERIES="2 3 4 6 8 9 12 15 22 23 24"
 
 mv ../results/results.csv ../results/results-`date +%s`.csv
 
+echo JAVA_OPTS: $JAVA_OPTS
 for SF in $SFS; do
 	for RUN in `seq 1 $RUNS`; do
 		for QUERY in $QUERIES; do
