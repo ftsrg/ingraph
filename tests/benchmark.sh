bc7b2b8144d3dbda2a1e157e22d195df1c36cc50
Gabor Szarnyas <szarnyasg@gmail.com>
2018-09-24 07:51:13 +0200

    Create separate directory for results

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -4,13 +4,13 @@ RUNS=3
 SFS="01 03 1"
 QUERIES="2 3 4 6 8 9 12 15 22 23 24"
 
-mv results.csv results-`date +%s`.csv
+mv ../results/results.csv ../results/results-`date +%s`.csv
 
 for SF in $SFS; do
 	for RUN in `seq 1 $RUNS`; do
 		for QUERY in $QUERIES; do
 			echo scale factor: $SF, run: $RUN, query: $QUERY
-			./tests $SF $QUERY | tee -a results.csv
+			./tests $SF $QUERY | tee -a ../results/results.csv
 		done
 	done
 done
