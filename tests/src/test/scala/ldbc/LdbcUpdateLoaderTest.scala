f27ca4ad29b5818b32190c079fa35d916b8c6701
Gabor Szarnyas <szarnyasg@gmail.com>
2018-10-05 19:41:01 +0200

    Load update specs in test

--- tests/src/test/scala/ldbc/LdbcUpdateLoaderTest.scala
+++ tests/src/test/scala/ldbc/LdbcUpdateLoaderTest.scala
@@ -3,14 +3,23 @@ package ldbc
 import ingraph.ire.Indexer
 import org.scalatest.FunSuite
 
+import scala.io.Source
+
 class LdbcUpdateLoaderTest extends FunSuite {
 
   val CSV_DIR: String = "../graphs/ldbc-snb-bi/sf01/"
+  val QUERY_PREFIX: String = "../queries/ldbc-snb-interactive/interactive-update-"
+  val QUERY_POSTFIX: String = ".cypher"
 
-  ignore("parse streams") {
+  test("parse streams") {
     val indexer = new Indexer()
     val loader = new LdbcUpdateLoader(indexer, CSV_DIR)
-    loader.load()
+    //loader.load()
+
+    for (query <- 1 to 8) {
+      val querySpecification = Source.fromFile(s"${QUERY_PREFIX}${query}${QUERY_POSTFIX}").getLines().mkString("\n")
+      println(querySpecification)
+    }
   }
 
 }
