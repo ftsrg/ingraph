1548695e37e5501cc814d338b05517529a1a2fa1
marci543 <marci543@users.noreply.github.com>
2018-10-07 18:51:36 +0200

    Config logging

--- cypher-to-sql/src/main/scala/ingraph/compiler/sql/driver/SqlDriver.scala
+++ cypher-to-sql/src/main/scala/ingraph/compiler/sql/driver/SqlDriver.scala
@@ -27,7 +27,6 @@ class SqlDriver(val translateCreateQueries: Boolean = false,
       Some(CypherDriverFactory.createNeo4jDriver())
   val backendSession: Option[Session] = backendDriver.map(_.session)
 
-  LogManager.getRootLogger.setLevel(Level.OFF)
   val url = database.url
 
   withResources(DriverManager.getConnection(url)) { sqlConnection =>
@@ -65,6 +64,7 @@ object SqlDriver {
   }
 
   class PostgresDatabase extends Database {
+    LogManager.getRootLogger.setLevel(Level.ERROR)
     val postgres = new EmbeddedPostgresWrapper
 
     override def url: String = postgres.Url
