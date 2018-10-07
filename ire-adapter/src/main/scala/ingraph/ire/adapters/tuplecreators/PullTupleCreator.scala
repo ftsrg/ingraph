4bd2ce7a4e4079c66377e991efcff986aa355c6c
János Maginecz <wafleeee@gmail.com>
2018-10-07 20:06:32 +0200

    Match getedges with bound src vertex ids

--- ire-adapter/src/main/scala/ingraph/ire/adapters/tuplecreators/PullTupleCreator.scala
+++ ire-adapter/src/main/scala/ingraph/ire/adapters/tuplecreators/PullTupleCreator.scala
@@ -39,11 +39,18 @@ class PullTupleCreator(vertexOps: Seq[GetVertices],
     val labels = operator.nnode.edge.labels.edgeLabels
     val edges: Iterable[IngraphEdge] = operator.nnode.edge.properties.get(TupleConstants.ID_KEY) match {
       case None =>
-        (for (label <- labels)
-          yield {
-            indexer.edgesByType(label).filter(e => sourceLabels.subsetOf(e.sourceVertex.labels)
-              && targetLabels.subsetOf(e.targetVertex.labels))
-          }).flatten
+        val unfiltered = operator.src.properties.get(TupleConstants.ID_KEY) match {
+          case Some(Literal(srcId,_)) =>
+            // TODO: benchmark if we should do this for trgId too
+            indexer.vertexLookup(srcId.asInstanceOf[Long]).edgesOut
+          case None =>
+            labels.flatMap(label =>
+                indexer.edgesByType(label)
+              )
+        }
+        unfiltered.filter(e =>
+          sourceLabels.subsetOf(e.sourceVertex.labels)
+            && targetLabels.subsetOf(e.targetVertex.labels))
       case Some(Literal(id, _)) =>
         val edge = indexer.edgeById(id.asInstanceOf[Long]).get
         assert(sourceLabels.subsetOf(edge.sourceVertex.labels)
